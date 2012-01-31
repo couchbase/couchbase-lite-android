272351eb0037df2d938b4d4e35e05e61239492ea
Marty Schoch <marty.schoch@gmail.com>
2012-01-31 12:16:57 -0500

    test app now starts a TDListener on 8888

--- TouchDB-Android-TestApp/src/com/couchbase/touchdb/testapp/TestAppActivity.java
+++ TouchDB-Android-TestApp/src/com/couchbase/touchdb/testapp/TestAppActivity.java
@@ -1,13 +1,34 @@
 package com.couchbase.touchdb.testapp;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.couchbase.touchdb.TDServer;
+import com.couchbase.touchdb.listener.TDListener;
 
 public class TestAppActivity extends Activity {
+
+    public static final String TAG = "TestAppActivity";
+    private TDListener listener;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        String filesDir = getFilesDir().getAbsolutePath();
+        TDServer server;
+        try {
+            server = new TDServer(filesDir);
+            listener = new TDListener(server, 8888);
+            listener.start();
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to create TDServer", e);
+        }
+
     }
 }
