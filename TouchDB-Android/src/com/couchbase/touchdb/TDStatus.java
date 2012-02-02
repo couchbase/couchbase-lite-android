76e653d8b149526144fa23126f84fa9148ee95f3
Marty Schoch <marty.schoch@gmail.com>
2012-02-02 10:09:36 -0500

    added code for METHOD_NOT_ALLOWED

--- TouchDB-Android/src/com/couchbase/touchdb/TDStatus.java
+++ TouchDB-Android/src/com/couchbase/touchdb/TDStatus.java
@@ -29,6 +29,7 @@ public class TDStatus {
     public static final int BAD_REQUEST = 400;
     public static final int FORBIDDEN = 403;
     public static final int NOT_FOUND = 404;
+    public static final int METHOD_NOT_ALLOWED = 405;
     public static final int NOT_ACCEPTABLE = 406;
     public static final int CONFLICT = 409;
     public static final int PRECONDITION_FAILED = 412;
