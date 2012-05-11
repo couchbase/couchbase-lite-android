8116b5a1555f70f2c1f11faffe5971fea0ed1a99
Marty Schoch <marty.schoch@gmail.com>
2012-05-10 17:28:36 -0700

    changed test address again

--- TouchDB-Android-TestApp/src/com/couchbase/touchdb/testapp/tests/TestConstants.java
+++ TouchDB-Android-TestApp/src/com/couchbase/touchdb/testapp/tests/TestConstants.java
@@ -2,6 +2,6 @@ package com.couchbase.touchdb.testapp.tests;
 
 public interface TestConstants {
 
-    String replicationURL = "http://10.17.4.125:5984/tdreplicator_test";
+    String replicationURL = "http://127.0.0.1:5984/tdreplicator_test";
 
 }
