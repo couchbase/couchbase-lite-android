4b83364dc4ab3b7c0c777203878a40c9d92d765e
Marty Schoch <marty.schoch@gmail.com>
2012-05-30 19:18:24 -0400

    added support for username/password in the URL for replication
    https://github.com/couchbaselabs/TouchDB-Android/issues/27

--- TouchDB-Android/src/com/couchbase/touchdb/support/TDRemoteRequest.java
+++ TouchDB-Android/src/com/couchbase/touchdb/support/TDRemoteRequest.java
@@ -5,9 +5,18 @@ import java.io.InputStream;
 import java.net.URL;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthState;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
@@ -15,7 +24,12 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.ExecutionContext;
+import org.apache.http.protocol.HttpContext;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import android.os.Handler;
@@ -71,7 +85,41 @@ public class TDRemoteRequest implements Runnable {
             request = new HttpPost(url.toExternalForm());
         }
 
+        // if the URL contains user info AND if this a DefaultHttpClient
+        // then preemptively set the auth credentials
+        if(url.getUserInfo() != null) {
+            if(url.getUserInfo().contains(":")) {
+                String[] userInfoSplit = url.getUserInfo().split(":");
+                final Credentials creds = new UsernamePasswordCredentials(userInfoSplit[0], userInfoSplit[1]);
+                if(httpClient instanceof DefaultHttpClient) {
+                    DefaultHttpClient dhc = (DefaultHttpClient)httpClient;
 
+                    HttpRequestInterceptor preemptiveAuth = new HttpRequestInterceptor() {
+
+                        @Override
+                        public void process(HttpRequest request,
+                                HttpContext context) throws HttpException,
+                                IOException {
+                            AuthState authState = (AuthState) context.getAttribute(ClientContext.TARGET_AUTH_STATE);
+                            CredentialsProvider credsProvider = (CredentialsProvider) context.getAttribute(
+                                    ClientContext.CREDS_PROVIDER);
+                            HttpHost targetHost = (HttpHost) context.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
+
+                            if (authState.getAuthScheme() == null) {
+                                AuthScope authScope = new AuthScope(targetHost.getHostName(), targetHost.getPort());
+                                authState.setAuthScheme(new BasicScheme());
+                                authState.setCredentials(creds);
+                            }
+                        }
+                    };
+
+                    dhc.addRequestInterceptor(preemptiveAuth, 0);
+                }
+            }
+            else {
+                Log.w(TDDatabase.TAG, "Unable to parse user info, not setting credentials");
+            }
+        }
 
         request.addHeader("Accept", "application/json");
 
