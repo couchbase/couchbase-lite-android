087705e33faa68b0d6d6326bde252437a2265986
Marty Schoch <marty.schoch@gmail.com>
2012-05-30 19:09:38 -0400

    update test constants to be a little more usable/understandable

--- TouchDB-Android-TestApp/src/com/couchbase/touchdb/testapp/tests/TestConstants.java
+++ TouchDB-Android-TestApp/src/com/couchbase/touchdb/testapp/tests/TestConstants.java
@@ -2,6 +2,9 @@ package com.couchbase.touchdb.testapp.tests;
 
 public interface TestConstants {
 
-    String replicationURL = "http://10.0.2.2:5984/tdreplicator_test";
+    //10.0.2.2 is your local machine from within the android emulator
+    String replicationServer = "http://10.0.2.2:5984";
+    String replicationURL = replicationServer + "/tdreplicator_test";
+
 
 }
