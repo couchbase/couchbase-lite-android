2c818835668d9a659cc5523ca228a1dd33a81b1b
Marty Schoch <mschoch@Marty-Schochs-Mac-Pro.local>
2012-08-12 17:59:35 -0400

    changes to unit tests and test activity for replication changes new test case for TDSequenceMap

--- TouchDB-Android-TestApp/src/com/couchbase/touchdb/testapp/TestAppActivity.java
+++ TouchDB-Android-TestApp/src/com/couchbase/touchdb/testapp/TestAppActivity.java
@@ -2,26 +2,52 @@ package com.couchbase.touchdb.testapp;
 
 import java.io.IOException;
 
+import org.ektorp.CouchDbInstance;
+import org.ektorp.ReplicationCommand;
+import org.ektorp.impl.StdCouchDbInstance;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 
 import com.couchbase.touchdb.TDServer;
+import com.couchbase.touchdb.ektorp.TouchDBHttpClient;
 import com.couchbase.touchdb.listener.TDListener;
+import com.couchbase.touchdb.router.TDURLStreamHandlerFactory;
 
 public class TestAppActivity extends Activity {
 
     public static final String TAG = "TestAppActivity";
     private TDListener listener;
+    private TDServer server;
+
+    {
+        TDURLStreamHandlerFactory.registerSelfIgnoreError();
+    }
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads().detectDiskWrites().detectNetwork() // or
+                                                                      // .detectAll()
+                                                                      // for all
+                                                                      // detectable
+                                                                      // problems
+                .penaltyLog().build());
+
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                // .detectLeakedClosableObjects()
+                .penaltyLog().penaltyDeath().build());
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
         String filesDir = getFilesDir().getAbsolutePath();
-        TDServer server;
         try {
             server = new TDServer(filesDir);
             listener = new TDListener(server, 8888);
@@ -30,5 +56,31 @@ public class TestAppActivity extends Activity {
             Log.e(TAG, "Unable to create TDServer", e);
         }
 
+        TouchDBHttpClient client = new TouchDBHttpClient(server);
+        CouchDbInstance instance = new StdCouchDbInstance(client);
+        instance.createConnector("tutorial", true);
+
+        ReplicationCommand pull = new ReplicationCommand.Builder()
+                .source("http://192.168.1.10:5984/itdashboard").target("tutorial")
+                .continuous(false).build();
+
+        instance.replicate(pull);
+    }
+
+    @Override
+    protected void onDestroy() {
+        AsyncTask<Void, Void, Void> closeServer = new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                if (server != null) {
+                    server.close();
+                }
+                return null;
+            };
+        };
+        closeServer.execute();
+
+        super.onDestroy();
     }
 }
