0637afa97289b224fe838557399b440f06373bd2
Traun Leyden <traun@couchbase.com>
2013-07-03 17:50:25 -0700

    script to start android emulator

--- CouchbaseLiteProject/start_android_emulator.sh
+++ CouchbaseLiteProject/start_android_emulator.sh
@@ -1,5 +1,15 @@
 
+
 # usage: pass in the name of the avd, eg:
 # ./start_android_emulator 3.7WVGA
 # you can get the list of avd names by running "android list avd"
+
+# make sure avd name was passed in
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+
+# start emulator
 emulator64-arm -avd $* -netspeed full -netdelay none
