fdb1188d3311830fbb11f155603adcec0acc0899
Traun Leyden <traun@couchbase.com>
2013-07-19 15:33:53 -0700

    Add empty test case for CBLListener

---
+++ CouchbaseLiteProject/CBLiteListener/src/instrumentTest/java/com/couchbase/cblite/testapp/listener/tests/CBLiteListenerTestCase.java
@@ -0,0 +1,8 @@
+package com.couchbase.cblite.testapp.listener.tests;
+
+import android.test.InstrumentationTestCase;
+
+public abstract class CBLiteListenerTestCase extends InstrumentationTestCase {
+
+
+}
