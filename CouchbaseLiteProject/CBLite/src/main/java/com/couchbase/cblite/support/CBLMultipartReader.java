201f6a8f931367e2c70373388efc84bde2eee8c1
Traun Leyden <traun@couchbase.com>
2013-07-31 16:27:19 -0700

    parses content type correctly.  issue #47

--- CouchbaseLiteProject/CBLite/src/main/java/com/couchbase/cblite/support/CBLMultipartReader.java
+++ CouchbaseLiteProject/CBLite/src/main/java/com/couchbase/cblite/support/CBLMultipartReader.java
@@ -1,8 +1,12 @@
 package com.couchbase.cblite.support;
 
+import java.nio.charset.Charset;
+import java.util.StringTokenizer;
+
 public class CBLMultipartReader {
 
     private String contentType;
+    private byte[] boundary;
     private CBLMultipartReaderDelegate delegate;
 
     public CBLMultipartReader(String contentType, CBLMultipartReaderDelegate delegate) {
@@ -15,8 +19,7 @@ public class CBLMultipartReader {
     }
 
     public byte[] getBoundary() {
-        // TODO
-        return new byte[1];
+        return boundary;
     }
 
     public boolean finished() {
@@ -29,7 +32,35 @@ public class CBLMultipartReader {
     }
 
     private void parseContentType() {
-        // TODO: parse content type
+
+        StringTokenizer tokenizer = new StringTokenizer(contentType, ";");
+        boolean first = true;
+        while (tokenizer.hasMoreTokens()) {
+            String param = tokenizer.nextToken().trim();
+            if (first == true) {
+                if (!param.startsWith("multipart/")) {
+                    throw new IllegalArgumentException(contentType + " does not start with multipart/");
+                }
+                first = false;
+            }
+            else {
+                if (param.startsWith("boundary=")) {
+                    String tempBoundary = param.substring(9);
+                    if (tempBoundary.startsWith("\"")) {
+                        if (tempBoundary.length() < 2 || !tempBoundary.endsWith("\"")) {
+                            throw new IllegalArgumentException(contentType + " is not valid");
+                        }
+                        tempBoundary = tempBoundary.substring(1, tempBoundary.length()-1);
+                    }
+                    if (tempBoundary.length() < 1) {
+                        throw new IllegalArgumentException(contentType + " has zero-length boundary");
+                    }
+                    tempBoundary = String.format("\r\n--%s", tempBoundary);
+                    boundary = tempBoundary.getBytes(Charset.forName("UTF-8"));
+                    break;
+                }
+            }
+        }
 
 
     }
