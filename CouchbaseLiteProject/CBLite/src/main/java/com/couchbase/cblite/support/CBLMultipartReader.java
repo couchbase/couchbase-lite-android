3463e0707a64aa7949aa3e84cb529565edc1b71b
Traun Leyden <traun@couchbase.com>
2013-07-31 18:41:51 -0700

    implement append().  issue #47

--- CouchbaseLiteProject/CBLite/src/main/java/com/couchbase/cblite/support/CBLMultipartReader.java
+++ CouchbaseLiteProject/CBLite/src/main/java/com/couchbase/cblite/support/CBLMultipartReader.java
@@ -1,18 +1,40 @@
 package com.couchbase.cblite.support;
 
+import org.apache.http.util.ByteArrayBuffer;
+
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class CBLMultipartReader {
 
+    private static enum CBLMultipartReaderState {
+        kUninitialized,
+        kAtStart,
+        kInPrologue,
+        kInBody,
+        kInHeaders,
+        kAtEnd,
+        kFailed
+    }
+    private static Charset utf8 = Charset.forName("UTF-8");
+    private static byte[] kCRLFCRLF = new String("\r\n\r\n").getBytes(utf8);
+
+    private CBLMultipartReaderState state;
+    private ByteArrayBuffer buffer;
     private String contentType;
     private byte[] boundary;
     private CBLMultipartReaderDelegate delegate;
+    private Map<String, String> headers;
 
     public CBLMultipartReader(String contentType, CBLMultipartReaderDelegate delegate) {
 
         this.contentType = contentType;
         this.delegate = delegate;
+        this.buffer = new ByteArrayBuffer(1024);
+        this.state = CBLMultipartReaderState.kAtStart;
 
         parseContentType();
 
@@ -22,13 +44,166 @@ public class CBLMultipartReader {
         return boundary;
     }
 
+    public byte[] getBoundaryWithoutLeadingCRLF() {
+        byte[] rawBoundary = getBoundary();
+        byte[] result = Arrays.copyOfRange(rawBoundary, 2, rawBoundary.length);
+        return result;
+    }
+
     public boolean finished() {
-        // TODO
-        return true;
+        return state == CBLMultipartReaderState.kAtEnd;
+    }
+
+    private byte[] eomBytes() {
+        return new String("--").getBytes(Charset.forName("UTF-8"));
+    }
+
+    private boolean memcmp(byte[] array1, byte[] array2, int len) {
+        boolean equals = true;
+        for (int i=0; i<len; i++) {
+            if (array1[i] != array2[i]) {
+                equals = false;
+            }
+        }
+        return equals;
+    }
+
+    private Range searchFor(byte[] pattern, int start) {
+
+        byte[] byteArrayToSearch;
+        if (start == 0) {
+            byteArrayToSearch = buffer.toByteArray();
+        }
+        else {
+            byteArrayToSearch = Arrays.copyOfRange(buffer.toByteArray(), start, buffer.length());
+        }
+
+        KMPMatch searcher = new KMPMatch();
+        int matchIndex = searcher.indexOf(byteArrayToSearch, pattern);
+        if (matchIndex > 0) {
+            return new Range(matchIndex, pattern.length);
+        }
+        else {
+            return new Range(matchIndex, 0);
+        }
+    }
+
+    private void parseHeaders(String headersStr) {
+
+        if (headersStr == null || headersStr.length() == 0) {
+            throw new IllegalArgumentException("Unparseable UTF-8 in headers");
+        }
+        headers = new HashMap<String, String>();
+        boolean first = true;
+        StringTokenizer tokenizer = new StringTokenizer(headersStr, "\r\n");
+        while (tokenizer.hasMoreTokens()) {
+            String header = tokenizer.nextToken();
+            if (first) {
+                first = false;  // first line is just the whitespace between separator and its CRLF
+            }
+            else {
+                if (header.indexOf(":") == -1) {
+                    throw new IllegalArgumentException("Missing ':' in header line: " + header);
+                }
+                StringTokenizer headerTokenizer  = new StringTokenizer(header, ":");
+                String key = headerTokenizer.nextToken().trim();
+                String value = headerTokenizer.nextToken().trim();
+                headers.put(key, value);
+            }
+        }
+
     }
 
     public void appendData(byte[] data) {
-        // TODO
+        System.out.println("appendData called");
+
+        if (buffer == null) {
+            return;
+        }
+        if (data.length == 0) {
+            return;
+        }
+        buffer.append(data, 0, data.length);
+
+        CBLMultipartReaderState nextState;
+        do {
+            nextState = CBLMultipartReaderState.kUninitialized;
+            int bufLen = buffer.length();
+            switch (state) {
+                case kAtStart: {
+                    // The entire message might start with a boundary without a leading CRLF.
+                    byte[] boundaryWithoutLeadingCRLF = getBoundaryWithoutLeadingCRLF();
+                    if (bufLen >= boundaryWithoutLeadingCRLF.length) {
+                        if (Arrays.equals(buffer.toByteArray(), boundaryWithoutLeadingCRLF)) {
+                            buffer.clear(); // TODO: broken!?
+                            nextState = CBLMultipartReaderState.kInHeaders;
+                        }
+                        else {
+                            nextState = CBLMultipartReaderState.kInPrologue;
+                        }
+                    }
+                    break;
+                }
+                case kInPrologue:
+                case kInBody: {
+                    // Look for the next part boundary in the data we just added and the ending bytes of
+                    // the previous data (in case the boundary string is split across calls)
+                    if (bufLen < boundary.length) {
+                        break;
+                    }
+                    int start = Math.max(0, bufLen - data.length - boundary.length);
+                    Range r = searchFor(boundary, start);
+                    if (r.getLength() > 0) {
+                        if (state == CBLMultipartReaderState.kInBody) {
+                            byte[] dataToAppend = Arrays.copyOfRange(buffer.toByteArray(), 0, r.getLocation());
+                            delegate.appendToPart(dataToAppend);
+                            delegate.finishedPart();
+                        }
+                        buffer.clear();  // TODO: broken!?
+                        nextState = CBLMultipartReaderState.kInHeaders;
+                    }
+                    break;
+                }
+                case kInHeaders: {
+                    // First check for the end-of-message string ("--" after separator):
+                    if (bufLen >= 2 &&
+                            memcmp(buffer.toByteArray(), eomBytes(), 2 )) {
+                        state = CBLMultipartReaderState.kAtEnd;
+                        close();
+                        return;
+                    }
+                    // Otherwise look for two CRLFs that delimit the end of the headers:
+                    Range r = searchFor(kCRLFCRLF, 0);
+                    if (r.getLength() > 0) {
+                        byte[] headersBytes = Arrays.copyOf(buffer.toByteArray(), r.getLocation());
+                        String headersString = new String(headersBytes, utf8);
+                        parseHeaders(headersString);
+                        buffer.clear();  // TODO: broken!?
+                        delegate.startedPart(headers);
+                        nextState = CBLMultipartReaderState.kInBody;
+
+                    }
+                    break;
+
+                }
+                default: {
+                    throw new IllegalStateException("Unexpected data after end of MIME body");
+                }
+            }
+
+            if (nextState != CBLMultipartReaderState.kUninitialized) {
+                state = nextState;
+            }
+
+        } while (nextState != CBLMultipartReaderState.kUninitialized && buffer.length() > 0);
+
+
+    }
+
+    private void close() {
+        buffer = null;
+        boundary = null;
+        state = CBLMultipartReaderState.kUninitialized;
     }
 
     private void parseContentType() {
@@ -68,4 +243,70 @@ public class CBLMultipartReader {
 
 
 
+}
+
+class Range {
+    private int location;
+    private int length;
+
+    Range(int location, int length) {
+        this.location = location;
+        this.length = length;
+    }
+
+    int getLocation() {
+        return location;
+    }
+
+    int getLength() {
+        return length;
+    }
+}
+
+/**
+ * Knuth-Morris-Pratt Algorithm for Pattern Matching
+ */
+class KMPMatch {
+    /**
+     * Finds the first occurrence of the pattern in the text.
+     */
+    public int indexOf(byte[] data, byte[] pattern) {
+        int[] failure = computeFailure(pattern);
+
+        int j = 0;
+        if (data.length == 0)
+            return -1;
+
+        for (int i = 0; i < data.length; i++) {
+            while (j > 0 && pattern[j] != data[i]) {
+                j = failure[j - 1];
+            }
+            if (pattern[j] == data[i]) { j++; }
+            if (j == pattern.length) {
+                return i - pattern.length + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Computes the failure function using a boot-strapping process,
+     * where the pattern is matched against itself.
+     */
+    private int[] computeFailure(byte[] pattern) {
+        int[] failure = new int[pattern.length];
+
+        int j = 0;
+        for (int i = 1; i < pattern.length; i++) {
+            while (j > 0 && pattern[j] != pattern[i]) {
+                j = failure[j - 1];
+            }
+            if (pattern[j] == pattern[i]) {
+                j++;
+            }
+            failure[i] = j;
+        }
+
+        return failure;
+    }
 }
