80984c0e9666e97a75c215a1f3bae323d0123315
Traun Leyden <traun@couchbase.com>
2013-08-22 17:18:21 -0700

    no need for this file

--- init_submodules.sh
+++
@@ -1,3 +0,0 @@
-git submodule add git@github.com:couchbase/CBLiteJavascript.git CouchbaseLiteProject/CBLiteJavascript
-git submodule add git@github.com:couchbase/CBLiteListener.git CouchbaseLiteProject/CBLiteListener
-git submodule add git@github.com:couchbase/CBLite.git CouchbaseLiteProject/CBLite
