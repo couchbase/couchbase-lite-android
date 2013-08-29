98b6f3e41da327fbf00f607076020b63e0a53bb7
Traun Leyden <traun@couchbase.com>
2013-08-28 19:44:29 -0700

    Unit test for issue #67

--- java/com/couchbase/cblite/testapp/tests/Server.java
+++ java/com/couchbase/cblite/testapp/tests/Server.java
@@ -1,10 +1,13 @@
 package com.couchbase.cblite.testapp.tests;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import junit.framework.Assert;
 
 import com.couchbase.cblite.CBLDatabase;
+import com.couchbase.cblite.CBLServer;
 
 public class Server extends CBLiteTestCase {
 
@@ -37,6 +40,36 @@ public class Server extends CBLiteTestCase {
 
         db.close();
         server.deleteDatabaseNamed("foo");
+    }
+
+    public void testUpgradeOldDatabaseFiles() throws Exception {
+
+
+        String directoryName = "test-directory-" + System.currentTimeMillis();
+        String normalFilesDir = getInstrumentation().getContext().getFilesDir().getAbsolutePath();
+        String fakeFilesDir = String.format("%s/%s", normalFilesDir, directoryName);
+
+        File directory = new File(fakeFilesDir);
+        if(!directory.exists()) {
+            boolean result = directory.mkdir();
+            if(!result) {
+                throw new IOException("Unable to create directory " + directory);
+            }
+        }
+        File oldTouchDbFile = new File(directory, String.format("old%s", CBLServer.DATABASE_SUFFIX_OLD));
+        oldTouchDbFile.createNewFile();
+        File newCbLiteFile = new File(directory, String.format("new%s", CBLServer.DATABASE_SUFFIX));
+        newCbLiteFile.createNewFile();
+
+        CBLServer serverForThisTest = new CBLServer(fakeFilesDir);
+
+        File migratedOldFile = new File(directory, String.format("old%s", CBLServer.DATABASE_SUFFIX));
+
+        Assert.assertTrue(migratedOldFile.exists());
+        Assert.assertTrue(oldTouchDbFile.exists() == false);
+        Assert.assertTrue(newCbLiteFile.exists());
+
+
     }
 
 }
