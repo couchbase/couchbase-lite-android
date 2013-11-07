adbce3ed9b981d85d592a150fce9af286c416ee2
Traun Leyden <traun@couchbase.com>
2013-11-06 16:41:13 -0800

    Move mockhttpclient to its own class

---
+++ java/com/couchbase/cblite/testapp/tests/MockHttpClient.java
@@ -0,0 +1,79 @@
+package com.couchbase.cblite.testapp.tests;
+
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+
+class MockHttpClient implements org.apache.http.client.HttpClient {
+
+    private int numTimesExecuteCalled = 0;
+
+    public int getNumTimesExecuteCalled() {
+        return numTimesExecuteCalled;
+    }
+
+    @Override
+    public HttpParams getParams() {
+        return null;
+    }
+
+    @Override
+    public ClientConnectionManager getConnectionManager() {
+        return null;
+    }
+
+    @Override
+    public HttpResponse execute(HttpUriRequest httpUriRequest) throws IOException, ClientProtocolException {
+        numTimesExecuteCalled++;
+        throw new IOException("Test IOException");
+    }
+
+    @Override
+    public HttpResponse execute(HttpUriRequest httpUriRequest, HttpContext httpContext) throws IOException, ClientProtocolException {
+        numTimesExecuteCalled++;
+        throw new IOException("Test IOException");
+    }
+
+    @Override
+    public HttpResponse execute(HttpHost httpHost, HttpRequest httpRequest) throws IOException, ClientProtocolException {
+        numTimesExecuteCalled++;
+        throw new IOException("Test IOException");
+    }
+
+    @Override
+    public HttpResponse execute(HttpHost httpHost, HttpRequest httpRequest, HttpContext httpContext) throws IOException, ClientProtocolException {
+        numTimesExecuteCalled++;
+        throw new IOException("Test IOException");
+    }
+
+    @Override
+    public <T> T execute(HttpUriRequest httpUriRequest, ResponseHandler<? extends T> responseHandler) throws IOException, ClientProtocolException {
+        throw new IOException("<T> Test IOException");
+    }
+
+    @Override
+    public <T> T execute(HttpUriRequest httpUriRequest, ResponseHandler<? extends T> responseHandler, HttpContext httpContext) throws IOException, ClientProtocolException {
+        throw new IOException("<T> Test IOException");
+    }
+
+    @Override
+    public <T> T execute(HttpHost httpHost, HttpRequest httpRequest, ResponseHandler<? extends T> responseHandler) throws IOException, ClientProtocolException {
+        throw new IOException("<T> Test IOException");
+    }
+
+    @Override
+    public <T> T execute(HttpHost httpHost, HttpRequest httpRequest, ResponseHandler<? extends T> responseHandler, HttpContext httpContext) throws IOException, ClientProtocolException {
+        throw new IOException("<T> Test IOException");
+    }
+
+
+}
