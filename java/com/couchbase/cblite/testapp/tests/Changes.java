06635a8e937e4b0c050bce953a44e26e77d8cc7c
Traun Leyden <traun@couchbase.com>
2013-11-12 18:22:46 -0800

    android.util.Log -> cblite.util.Log

--- java/com/couchbase/cblite/testapp/tests/Changes.java
+++ java/com/couchbase/cblite/testapp/tests/Changes.java
@@ -1,13 +1,12 @@
 package com.couchbase.cblite.testapp.tests;
 
-import android.util.Log;
-
 import com.couchbase.cblite.CBLDatabase;
 import com.couchbase.cblite.CBLDatabaseChangedFunction;
 import com.couchbase.cblite.CBLStatus;
 import com.couchbase.cblite.CBLiteException;
 import com.couchbase.cblite.internal.CBLBody;
 import com.couchbase.cblite.internal.CBLRevisionInternal;
+import com.couchbase.cblite.util.Log;
 
 import junit.framework.Assert;
 
