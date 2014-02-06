e9343e86e5325418aca064a97ac55eb5080f013f
Matt Quinn <matt@mjquinn.ca>
2014-02-05 21:45:28 -0500

    add failing test for DocumentChange.isExternal() flag

--- java/com/couchbase/lite/ChangesTest.java
+++ java/com/couchbase/lite/ChangesTest.java
@@ -3,6 +3,7 @@ package com.couchbase.lite;
 import com.couchbase.lite.internal.Body;
 import com.couchbase.lite.internal.RevisionInternal;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,6 +35,46 @@ public class ChangesTest extends LiteTestCase {
         Status status = new Status();
         rev1 = database.putRevision(rev1, null, false, status);
 
+        assertEquals(1, changeNotifications);
+    }
+
+    public void testLocalChangesAreNotExternal() throws CouchbaseLiteException {
+        changeNotifications = 0;
+        Database.ChangeListener changeListener = new Database.ChangeListener() {
+            @Override public void changed(Database.ChangeEvent event) {
+                changeNotifications++;
+                assertFalse(event.isExternal());
+            }
+        };
+        database.addChangeListener(changeListener);
+
+        // Insert a document locally.
+        Document document = database.createDocument();
+        document.createRevision().save();
+
+        // Make sure that the assertion in changeListener was called.
+        assertEquals(1, changeNotifications);
+    }
+
+    public void testPulledChangesAreExternal() throws CouchbaseLiteException {
+        changeNotifications = 0;
+        Database.ChangeListener changeListener = new Database.ChangeListener() {
+            @Override public void changed(Database.ChangeEvent event) {
+                changeNotifications++;
+                assertTrue(event.isExternal());
+            }
+        };
+        database.addChangeListener(changeListener);
+
+        // Insert a document as if it came from a remote source.
+        RevisionInternal rev = new RevisionInternal("docId", "1-rev", false, database);
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put("_id", rev.getDocId());
+        properties.put("_rev", rev.getRevId());
+        rev.setProperties(properties);
+        database.forceInsert(rev, Arrays.asList(rev.getRevId()), getReplicationURL());
+
+        // Make sure that the assertion in changeListener was called.
         assertEquals(1, changeNotifications);
     }
 
