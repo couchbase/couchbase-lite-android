1c8f7b4eef10caab99a7f60cbb4cdfe27846db2c
Traun Leyden <traun@couchbase.com>
2014-03-13 15:00:14 -0700

    Issue #96 - add lower level test to detect issue.
    https://github.com/couchbase/couchbase-lite-java-core/issues/96

--- src/instrumentTest/java/com/couchbase/lite/BlobStoreWriterTest.java
+++ src/instrumentTest/java/com/couchbase/lite/BlobStoreWriterTest.java
@@ -1,5 +1,7 @@
 package com.couchbase.lite;
 
+import com.couchbase.lite.util.Log;
+
 import junit.framework.Assert;
 
 import org.apache.commons.io.IOUtils;
@@ -15,15 +17,34 @@ public class BlobStoreWriterTest extends LiteTestCase {
         InputStream attachmentStream = getAsset("attachment.png");
         byte[] bytes = IOUtils.toByteArray(attachmentStream);
 
-        com.couchbase.lite.BlobStoreWriter blobStoreWriter = new com.couchbase.lite.BlobStoreWriter(attachments);
+        BlobStoreWriter blobStoreWriter = new BlobStoreWriter(attachments);
         blobStoreWriter.appendData(bytes);
         blobStoreWriter.finish();
         blobStoreWriter.install();
 
         String sha1DigestKey = blobStoreWriter.sHA1DigestString();
 
+        assertTrue(sha1DigestKey.contains("LmsoqJJ6LOn4YS60pYnvrKbBd64="));
+
         BlobKey keyFromSha1 = new BlobKey(sha1DigestKey);
         Assert.assertTrue(attachments.getSizeOfBlob(keyFromSha1) == bytes.length);
+
+    }
+
+    public void testBlobStoreWriterForBody() throws Exception {
+
+        BlobStore attachments = database.getAttachments();
+
+        InputStream attachmentStream = getAsset("attachment.png");
+
+        BlobStoreWriter blobStoreWriter = Attachment.blobStoreWriterForBody(attachmentStream, database);
+
+        String sha1DigestKey = blobStoreWriter.sHA1DigestString();
+
+        assertTrue(sha1DigestKey.contains("LmsoqJJ6LOn4YS60pYnvrKbBd64="));
+
+
+
 
     }
 
