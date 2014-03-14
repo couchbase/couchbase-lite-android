b5255d177c52066b989d249002b4ae3f57314310
Andrew Reslan <andrew.reslan@mac.com>
2014-03-14 16:55:07 +0000

    Added two test for manager.replaceDatabase, one without attachments and one with

--- src/instrumentTest/java/com/couchbase/lite/ManagerTest.java
+++ src/instrumentTest/java/com/couchbase/lite/ManagerTest.java
@@ -1,10 +1,20 @@
 package com.couchbase.lite;
 
+import com.couchbase.lite.util.Log;
+
+import junit.framework.Assert;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 public class ManagerTest extends LiteTestCase {
+
+    public static final String TAG = "ManagerTest";
 
     public void testServer() throws CouchbaseLiteException {
 
@@ -82,4 +92,125 @@ public class ManagerTest extends LiteTestCase {
 
     }
 
+    public void testReplaceDatabaseNamedNoAttachments() throws CouchbaseLiteException {
+
+        //Copy database from assets to local storage
+
+        InputStream dbStream = getAsset("noattachments.cblite");
+
+        //InputStream dbStream = getAssets().open("noattachments.cblite");
+
+        File tempDB = new File(getCacheDirectory(), "temp.cblite");
+
+        try
+        {
+            FileOutputStream tempOutput = new FileOutputStream(tempDB);
+
+            copyFile(dbStream, tempOutput);
+        }
+        catch(FileNotFoundException fnfex)
+        {
+            Log.e(TAG,"Failed to open temp DB file",fnfex);
+            fail();
+        }
+        catch(IOException ioex)
+        {
+            Log.e(TAG,"Failed to copy asset to temp DB file",ioex);
+            fail();
+        }
+
+        manager.replaceDatabase("replaced", tempDB, null);
+
+        //Now validate the number of files in the DB
+        assertEquals(10,manager.getDatabase("replaced").getDocumentCount());
+
+    }
+
+    public void testReplaceDatabaseNamedWithAttachments() throws CouchbaseLiteException {
+
+        InputStream dbStream = getAsset("withattachments.cblite");
+
+        //InputStream dbStream = getAssets().open("withattachments.cblite");
+
+        //Copy DB from assets to local file system, we need to
+        //do this as replaceDatabase takes java.io.File arguments
+        //and we can't generate these for bundled assets
+
+        File tempDB = new File(getCacheDirectory(), "temp.cblite");
+
+        try
+        {
+            FileOutputStream tempOutput = new FileOutputStream(tempDB);
+
+            copyFile(dbStream, tempOutput);
+        }
+        catch(FileNotFoundException fnfex)
+        {
+            Log.e(TAG,"Failed to open temp DB file",fnfex);
+            fail();
+        }
+        catch(IOException ioex)
+        {
+            Log.e(TAG,"Failed to copy asset to temp DB file",ioex);
+            fail();
+        }
+
+        //Copy attachments from assets to local file system
+
+        //Copy attachment blob from assets to local storage
+        InputStream blobStream = getAsset("attachments/2e6b28a8927a2ce9f8612eb4a589efaca6c177ae.blob");
+
+        File tempAttachmentsDir = new File(getCacheDirectory(), "attachments");
+
+        tempAttachmentsDir.mkdir();
+
+        File tempBlobPath = new File(tempAttachmentsDir, "2e6b28a8927a2ce9f8612eb4a589efaca6c177ae.blob");
+
+        try
+        {
+            FileOutputStream tempOutput = new FileOutputStream(tempBlobPath);
+
+            copyFile(blobStream, tempOutput);
+        }
+        catch(FileNotFoundException fnfex)
+        {
+            Log.e(TAG,"Failed to open temp blob file",fnfex);
+            fail();
+        }
+        catch(IOException ioex)
+        {
+            Log.e(TAG,"Failed to copy asset to temp blob file",ioex);
+            fail();
+        }
+
+        manager.replaceDatabase("replaced2", tempDB, tempAttachmentsDir);
+
+        //Validate the number of files in the DB
+        assertEquals(1,manager.getDatabase("replaced2").getDocumentCount());
+
+        //get the attachment from the document
+        Document doc = manager.getDatabase("replaced2").getExistingDocument("doc0-1394815387949");
+
+        assertNotNull(doc);
+
+        Attachment attachment = doc.getCurrentRevision().getAttachment("attachment.png");
+
+        Assert.assertEquals(519173L, attachment.getLength());
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+
+    protected File getCacheDirectory() {
+        String rootDirectoryPath = System.getProperty("user.dir");
+        File rootDirectory = new File(rootDirectoryPath);
+        rootDirectory = new File(rootDirectory, "data/data/com.couchbase.cblite.test/cache");
+
+        return rootDirectory;
+    }
 }
