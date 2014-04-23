f33e42de3cc6b1adb6e4314a14c0a09678b2cebf
Traun Leyden <traun@couchbase.com>
2014-04-23 15:23:17 -0700

    Rename com.couchbase.cblite -> com.couchbase.lite

--- src/instrumentTest/java/com/couchbase/lite/LiteTestContext.java
+++ src/instrumentTest/java/com/couchbase/lite/LiteTestContext.java
@@ -32,7 +32,7 @@ public class LiteTestContext implements Context {
     public File getRootDirectory() {
         String rootDirectoryPath = System.getProperty("user.dir");
         File rootDirectory = new File(rootDirectoryPath);
-        rootDirectory = new File(rootDirectory, "data/data/com.couchbase.cblite.test/files");
+        rootDirectory = new File(rootDirectory, "data/data/com.couchbase.lite.test/files");
 
         return rootDirectory;
     }
