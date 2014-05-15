25e18a976b9020dc509a862bbe51c99fc87fca94
unknown <yarong@yarong-t440p.redmond.corp.microsoft.com>
2014-05-14 17:47:44 -0700

    Fix bugs in previous PR & got more tests to pass
    build.gradle - Cleaning up declaration of listener dependency so it works
    in Couchbase's environment
    
    LiteTestCase - Get rid of logic that deleted test files from here to
    context.
    
    LiteTestContext - Allows caller to decide if subdirectory with data should
    be deleted before test is run. By default the answer is yes.
    
    Test9_LoadDB - The code assumed that DEFAULT_TEST_DB exists but since we
    are re-using the same instance of LiteListener (due to TJWS bugs) we can't
    rely on that so we create the DB if it's not there.

--- src/androidTest/java/com/couchbase/test/lite/LiteTestContextBase.java
+++ src/androidTest/java/com/couchbase/test/lite/LiteTestContextBase.java
@@ -13,8 +13,11 @@ public class LiteTestContextBase {
 
     public LiteTestContextBase() {
         rootDirectory = new File(System.getProperty("user.dir"), "data/data/com.couchbase.lite.test/files");
-        FileDirUtils.deleteRecursive(rootDirectory);
-        if (rootDirectory.mkdirs() == false) {
+        // The assumption is that rootDirectory is just a container and all 'real' work will happen in a subdirectory.
+        // Which means that clean up for a test should involve deleting the sub-directory, not the root directory since
+        // individual tests can (and do) use multiple sub-directories. If we keep deleting the root directory then when
+        // multiple contexts are created in different sub-directories they keep deleting each other!
+        if (rootDirectory.exists() == false && rootDirectory.mkdir() == false ) {
             throw new RuntimeException("Couldn't create temporary directory for listener!");
         }
     }
