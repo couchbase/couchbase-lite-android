9171c02a632dc0e7825882dbf460da054602f57e
Traun Leyden <tleyden@couchbase.com>
2014-06-05 15:23:35 -0700

    Revert "Revert "Fix bugs in previous PR & got more tests to pass""
    This reverts commit 151928cb121f846e0abfde3d34baa139a697704a.

--- src/androidTest/java/com/couchbase/lite/LiteTestCase.java
+++ src/androidTest/java/com/couchbase/lite/LiteTestCase.java
@@ -72,10 +72,6 @@ public abstract class LiteTestCase extends LiteTestCaseBase {
 
     protected void startCBLite() throws IOException {
         LiteTestContext context = new LiteTestContext();
-        String serverPath = context.getRootDirectory().getAbsolutePath();
-        File serverPathFile = new File(serverPath);
-        FileDirUtils.deleteRecursive(serverPathFile);
-        serverPathFile.mkdir();
         Manager.enableLogging(Log.TAG, Log.VERBOSE);
         Manager.enableLogging(Log.TAG_SYNC, Log.VERBOSE);
         Manager.enableLogging(Log.TAG_QUERY, Log.VERBOSE);
@@ -104,9 +100,9 @@ public abstract class LiteTestCase extends LiteTestCaseBase {
         // keeps the listener from stopping even when you tell it to stop.
         if (testListener == null) {
             LiteTestContext context = new LiteTestContext("testlistener");
-            Manager manager = new Manager(context, Manager.DEFAULT_OPTIONS);
-            manager.getDatabase(getReplicationDatabase());
-            testListener = new LiteListener(manager, getReplicationPort(), null);
+            Manager listenerManager = new Manager(context, Manager.DEFAULT_OPTIONS);
+            listenerManager.getDatabase(getReplicationDatabase());
+            testListener = new LiteListener(listenerManager, getReplicationPort(), null);
             testListener.start();
         }
     }
@@ -116,7 +112,7 @@ public abstract class LiteTestCase extends LiteTestCaseBase {
         return database;
     }
 
-    protected void stopDatabse() {
+    protected void stopDatabase() {
         if(database != null) {
             database.close();
         }
@@ -196,7 +192,7 @@ public abstract class LiteTestCase extends LiteTestCaseBase {
     protected void tearDown() throws Exception {
         Log.v(TAG, "tearDown");
         super.tearDown();
-        stopDatabse();
+        stopDatabase();
         stopCBLite();
     }
 
