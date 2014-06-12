9d07dfbca05517503790380df90035f88545b1e8
Matt Quinn <matt@mjquinn.ca>
2014-06-12 11:30:23 -0400

    add test for immutability of CBL's returned property maps
    See https://github.com/couchbase/couchbase-lite-java-core/issues/226.
    
    Fails without
    https://github.com/couchbase/couchbase-lite-java-core/pull/228.

--- src/androidTest/java/com/couchbase/lite/DocumentTest.java
+++ src/androidTest/java/com/couchbase/lite/DocumentTest.java
@@ -155,4 +155,50 @@ public class DocumentTest extends LiteTestCase {
 
     }
 
+    public void testDocumentPropertiesAreImmutable() throws Exception {
+        String jsonString = "{\n" +
+                "    \"name\":\"praying mantis\",\n" +
+                "    \"wikipedia\":{\n" +
+                "        \"behavior\":{\n" +
+                "            \"style\":\"predatory\",\n" +
+                "            \"attack\":\"ambush\"\n" +
+                "        },\n" +
+                "        \"evolution\":{\n" +
+                "            \"ancestor\":\"proto-roaches\",\n" +
+                "            \"cousin\":\"termite\"\n" +
+                "        }       \n" +
+                "    }   \n" +
+                "\n" +
+                "}";
+        Map map = (Map) Manager.getObjectMapper().readValue(jsonString, Object.class);
+        Document doc = createDocumentWithProperties(database, map);
+
+        boolean firstLevelImmutable = false;
+        Map<String, Object> props = doc.getProperties();
+        try {
+            props.put("name", "bug");
+        } catch (UnsupportedOperationException e) {
+            firstLevelImmutable = true;
+        }
+        assertTrue(firstLevelImmutable);
+
+        boolean secondLevelImmutable = false;
+        Map wikiProps = (Map) props.get("wikipedia");
+        try {
+            wikiProps.put("behavior", "unknown");
+        } catch (UnsupportedOperationException e) {
+            secondLevelImmutable = true;
+        }
+        assertTrue(secondLevelImmutable);
+
+        boolean thirdLevelImmutable = false;
+        Map evolutionProps = (Map) wikiProps.get("behavior");
+        try {
+            evolutionProps.put("movement", "flight");
+        } catch (UnsupportedOperationException e) {
+            thirdLevelImmutable = true;
+        }
+        assertTrue(thirdLevelImmutable);
+    }
+
 }
