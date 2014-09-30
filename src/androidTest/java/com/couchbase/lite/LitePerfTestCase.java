couchbase/couchbase-lite-android

commit 84b5842e0ffdd55b76452a0245452fd0e6f28608

Add three new test cases

src/androidTest/java/com/couchbase/lite/LitePerfTestCase.java
@@ -46,6 +46,10 @@ public abstract class LitePerfTestCase extends LiteTestCase {
     public String replicationServer;
     public int replicationPort;
     public String replicationDatabase;
+    public static final String _propertyValue = "1";
+    //If a test return this number, it means the test completed, but there is an error
+    //For example, if the replication tests does not get the same number of document as created
+    public static final int failingPerfNumber = 999999;
 
     @Override
     protected void setUp() throws Exception {
@@ -55,7 +59,7 @@ public abstract class LitePerfTestCase extends LiteTestCase {
         runMultiple();
     }
 
-    public double runOne(int numberOfDocuments, int sizeOfDocuments)  throws Exception {
+    public double runOne(int numberOfDocuments, int sizeOfDocuments)  throws CouchbaseLiteException {
         return 0;
     };
 
@@ -211,13 +215,16 @@ public abstract class LitePerfTestCase extends LiteTestCase {
                         if (ExecutionTime < min) min = ExecutionTime;
                         if (ExecutionTime > max) max = ExecutionTime;
                     } catch (CouchbaseLiteException ex) {
+                        Log.v("PerformanceStats", "Got CouchbaseLiteException. " + ex);
                         ex.printStackTrace();
                         fail();
                     } catch(InterruptedException ex) {
-                        Log.e(TAG, "Fail in Thread.sleep()", ex);
+                        Log.v("PerformanceStats", "Got InterruptedException. " + ex);
+                        ex.printStackTrace();
                         fail();
                     } catch(Exception ex) {
-                        Log.e(TAG, "Fail to teardown and restart db", ex);
+                        Log.v("PerformanceStats", "Got Exception. " + ex);
+                        ex.printStackTrace();
                         fail();
                     }
                 }

src/androidTest/java/com/couchbase/lite/performance2/Test28_KeySizes.java
@@ -1,0 +1,122 @@
+/**
+ * Original iOS version by  Jens Alfke
+ * Ported to Android by Marty Schoch
+ *
+ * Copyright (c) 2012 Couchbase, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package com.couchbase.lite.performance2;
+
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.Emitter;
+import com.couchbase.lite.LitePerfTestCase;
+import com.couchbase.lite.LiteTestCase;
+import com.couchbase.lite.Mapper;
+import com.couchbase.lite.Query;
+import com.couchbase.lite.QueryEnumerator;
+import com.couchbase.lite.QueryRow;
+import com.couchbase.lite.Reducer;
+import com.couchbase.lite.Status;
+import com.couchbase.lite.TransactionalTask;
+import com.couchbase.lite.View;
+import com.couchbase.lite.internal.Body;
+import com.couchbase.lite.internal.RevisionInternal;
+import com.couchbase.lite.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//This test case is similar to Test13_QueryView, But it measures the time for create docs, define view, and do query
+// This is to compare with Android's test case of similar name
+public class Test28_KeySizes extends LitePerfTestCase {
+
+    public static final String TAG = "Test28_KeySizes";
+    private static final String _propertyValue = "1";
+
+    public double runOne(final int numberOfDocuments, final int sizeOfDocuments) throws CouchbaseLiteException {
+        final StringBuffer bigObj = new StringBuffer(sizeOfDocuments);
+        for (int i = 0; i < sizeOfDocuments; i++) {
+            bigObj.append(_propertyValue);
+        }
+
+        //Start measurement, including create docs, define view, and do query
+        long startMillis = System.currentTimeMillis();
+
+        boolean success = database.runInTransaction(new TransactionalTask() {
+
+            public boolean run() {
+                for (int i = 0; i < numberOfDocuments; i++) {
+                    String name = String.format("%s",bigObj);
+                    boolean vacant = ((i + 2) % 2 == 0) ? true : false;
+                    Map<String,Object> props = new HashMap<String,Object>();
+
+                    props.put("name",name);
+                    props.put("apt",i);
+                    props.put("phone",408100000 + i);
+                    props.put("vacant",vacant);
+
+                    Document doc = database.createDocument();
+                    try {
+                        doc.putProperties(props);
+                    }
+                    catch(CouchbaseLiteException cblex)
+                    {
+                        Log.v("PerformanceStats",TAG+", Failed to create doc "+props,cblex);
+                        return false;
+                    }
+                }
+                return true;
+            }
+        });
+
+        View view = database.getView("vacant");
+        view.setMapReduce(
+                new Mapper() {
+                    public void map(Map<String, Object> document, Emitter emitter) {
+                        Boolean vacant = (Boolean) document.get("vacant");
+                        String name = (String) document.get("name");
+                        if (vacant && name != null) {
+                            emitter.emit(name, vacant);
+                        }
+                    }
+                },
+                new Reducer() {
+                    public Object reduce(List<Object> keys, List<Object> values, boolean rereduce) {
+                        return View.totalValues(values);
+                    }
+                },
+                "1.0.0"
+        );
+
+        Query query = database.getView("vacant").createQuery();
+        query.setDescending(false);
+        query.setMapOnly(true);
+
+        QueryEnumerator rowEnum = query.run();
+        Object key;
+        Object value;
+        while (rowEnum.hasNext()) {
+            QueryRow row = rowEnum.next();
+            key = (String)row.getKey();
+            value = (Boolean)row.getValue();
+        }
+
+        double executionTime = Long.valueOf(System.currentTimeMillis()-startMillis);
+        Log.v("PerformanceStats",TAG+","+executionTime+","+numberOfDocuments+","+sizeOfDocuments);
+        return executionTime;
+    }
+}
+

src/androidTest/java/com/couchbase/lite/performance2/Test29_AllDocQuery.java
@@ -1,0 +1,103 @@
+/**
+ * Original iOS version by  Jens Alfke
+ * Ported to Android by Marty Schoch
+ *
+ * Copyright (c) 2012 Couchbase, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package com.couchbase.lite.performance2;
+
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.Emitter;
+import com.couchbase.lite.LitePerfTestCase;
+import com.couchbase.lite.LiteTestCase;
+import com.couchbase.lite.Mapper;
+import com.couchbase.lite.Query;
+import com.couchbase.lite.Query.AllDocsMode;
+import com.couchbase.lite.QueryEnumerator;
+import com.couchbase.lite.QueryRow;
+import com.couchbase.lite.Reducer;
+import com.couchbase.lite.Status;
+import com.couchbase.lite.TransactionalTask;
+import com.couchbase.lite.View;
+import com.couchbase.lite.internal.Body;
+import com.couchbase.lite.internal.RevisionInternal;
+import com.couchbase.lite.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//This test case is similar to Test13_QueryView, But it measures the time for create docs, define view, and do query
+// This is to compare with Android's test case of similar name
+public class Test29_AllDocQuery extends LitePerfTestCase {
+
+    public static final String TAG = "Test29_AllDocQuery.java";
+    private static final String _propertyValue = "1";
+
+    public double runOne(final int numberOfDocuments, final int sizeOfDocuments) throws CouchbaseLiteException {
+        final StringBuffer bigObj = new StringBuffer(sizeOfDocuments);
+        for (int i = 0; i < sizeOfDocuments; i++) {
+            bigObj.append(_propertyValue);
+        }
+
+        boolean success = database.runInTransaction(new TransactionalTask() {
+
+            public boolean run() {
+                for (int i = 0; i < numberOfDocuments; i++) {
+                    String name = String.format("%s",bigObj);
+                    boolean vacant = ((i + 2) % 2 == 0) ? true : false;
+                    Map<String,Object> props = new HashMap<String,Object>();
+
+                    props.put("name",name);
+                    props.put("apt",i);
+                    props.put("phone",408100000 + i);
+                    props.put("vacant",vacant);
+
+                    Document doc = database.createDocument();
+                    try {
+                        doc.putProperties(props);
+                    }
+                    catch(CouchbaseLiteException cblex)
+                    {
+                        Log.v("PerformanceStats",TAG+", Failed to create doc "+props,cblex);
+                        return false;
+                    }
+                }
+                return true;
+            }
+        });
+
+        //Start measurement, including create docs, define view, and do query
+        long startMillis = System.currentTimeMillis();
+
+        Query query = database.createAllDocumentsQuery();
+        query.setAllDocsMode(AllDocsMode.INCLUDE_DELETED);
+
+        QueryEnumerator rowEnum = query.run();
+        Object key;
+        Object value;
+        while (rowEnum.hasNext()) {
+            QueryRow row = rowEnum.next();
+            key = (String)row.getKey();
+            value = row.getValue();
+        }
+
+        double executionTime = Long.valueOf(System.currentTimeMillis()-startMillis);
+        Log.v("PerformanceStats",TAG+","+executionTime+","+numberOfDocuments+","+sizeOfDocuments);
+        return executionTime;
+    }
+}
+

src/androidTest/java/com/couchbase/lite/performance2/Test30_LiveQuery.java
@@ -1,0 +1,174 @@
+/**
+ * Original iOS version by  Jens Alfke
+ * Ported to Android by Marty Schoch
+ *
+ * Copyright (c) 2012 Couchbase, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package com.couchbase.lite.performance2;
+
+import com.couchbase.lite.AsyncTask;
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.Emitter;
+import com.couchbase.lite.LitePerfTestCase;
+import com.couchbase.lite.LiteTestCase;
+import com.couchbase.lite.LiveQuery;
+import com.couchbase.lite.Mapper;
+import com.couchbase.lite.Query;
+import com.couchbase.lite.QueryEnumerator;
+import com.couchbase.lite.QueryRow;
+import com.couchbase.lite.Reducer;
+import com.couchbase.lite.Status;
+import com.couchbase.lite.TransactionalTask;
+import com.couchbase.lite.View;
+import com.couchbase.lite.internal.Body;
+import com.couchbase.lite.internal.RevisionInternal;
+import com.couchbase.lite.replicator.Replication;
+import com.couchbase.lite.util.Log;
+
+import junit.framework.Assert;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class Test30_LiveQuery extends LitePerfTestCase {
+
+    public static final String TAG = "Test30_LiveQuery";
+
+    public double runOne(final int numberOfDocuments, final int sizeOfDocuments) throws CouchbaseLiteException {
+        final CountDownLatch doneSignal = new CountDownLatch(numberOfDocuments);
+
+        // run a live query
+        View view = database.getView("vu");
+        view.setMap(new Mapper() {
+            @Override
+            public void map(Map<String, Object> document, Emitter emitter) {
+                emitter.emit(document.get("sequence"), null);
+            }
+        }, "1");
+        final LiveQuery query = view.createQuery().toLiveQuery();
+        Log.i(TAG, "Created  " + query);
+
+        // install a change listener which decrements countdown latch when it sees a new
+        // key from the list of expected keys
+        final LiveQuery.ChangeListener changeListener = new LiveQuery.ChangeListener() {
+            @Override
+            public void changed(LiveQuery.ChangeEvent event) {
+                QueryEnumerator rows = event.getRows();
+                int count = rows.getCount();
+                //Log.v("PerformanceStats",TAG+", In ChangeListener, got " + rows.getCount() + " rows.");
+                if (count == numberOfDocuments) {
+                    doneSignal.countDown();
+                }
+            }
+        };
+        query.addChangeListener(changeListener);
+
+        //Start measurement, including create docs, define view, and do query
+        long startMillis = System.currentTimeMillis();
+
+        // create the docs that will cause the above change listener to decrement countdown latch
+        createDocumentsAsync(database, numberOfDocuments, sizeOfDocuments);
+
+        query.run();  // this will block until the query completes
+
+        // wait for the doneSignal to be finished
+        try {
+            boolean success = doneSignal.await(5, TimeUnit.SECONDS);  //??? 300
+        }
+        catch(InterruptedException ex)
+        {
+            Log.v("PerformanceStats",TAG+", Got exception during doneSignal.await. "+ ex);
+            return 0;
+        }
+
+        // stop the live query since we are done with it
+        query.removeChangeListener(changeListener);
+        query.stop();
+
+        double executionTime = Long.valueOf(System.currentTimeMillis()-startMillis);
+        Log.v("PerformanceStats",TAG+","+executionTime+","+numberOfDocuments+","+sizeOfDocuments);
+        if (query.getRows().getCount() == numberOfDocuments) {
+            return executionTime;
+        } else {
+            Log.v("PerformanceStats", TAG + ", Number of docs return by query:" + query.getRows().getCount() + ". Expecting:" + numberOfDocuments);
+            return failingPerfNumber;
+        }
+    }
+
+    public static void createDocuments(final Database db, final int numberOfDocuments, final int sizeOfDocuments) {
+        final StringBuffer bigObj = new StringBuffer(sizeOfDocuments);
+        for (int i = 0; i < sizeOfDocuments; i++) {
+            bigObj.append(_propertyValue);
+        }
+        String name = String.format("%s",bigObj);
+
+        for (int i=0; i<numberOfDocuments; i++) {
+            Map<String,Object> properties = new HashMap<String,Object>();
+            properties.put("name", name);
+            properties.put("sequence", i);
+            createDocumentWithProperties(db, properties);
+        }
+    };
+
+    static Future createDocumentsAsync(final Database db, final int numberOfDocuments, final int sizeOfDocuments) {
+        return db.runAsync(new AsyncTask() {
+            @Override
+            public void run(Database database) {
+                database.beginTransaction();
+                createDocuments(db, numberOfDocuments, sizeOfDocuments);
+                database.endTransaction(true);
+            }
+        });
+
+    };
+
+
+    public static Document createDocumentWithProperties(Database db, Map<String,Object>  properties) {
+        Document  doc = db.createDocument();
+        Assert.assertNotNull(doc);
+        Assert.assertNull(doc.getCurrentRevisionId());
+        Assert.assertNull(doc.getCurrentRevision());
+        Assert.assertNotNull("Document has no ID", doc.getId()); // 'untitled' docs are no longer untitled (8/10/12)
+        try{
+            doc.putProperties(properties);
+        } catch( Exception e){
+            Log.e(TAG, "Error creating document", e);
+            assertTrue("can't create new document in db:" + db.getName() + " with properties:" + properties.toString(), false);
+        }
+        Assert.assertNotNull(doc.getId());
+        Assert.assertNotNull(doc.getCurrentRevisionId());
+        Assert.assertNotNull(doc.getUserProperties());
+
+        // should be same doc instance, since there should only ever be a single Document instance for a given document
+        Assert.assertEquals(db.getDocument(doc.getId()), doc);
+
+        Assert.assertEquals(db.getDocument(doc.getId()).getId(), doc.getId());
+
+        return doc;
+    }
+
+
+
+}
+

commit 6587639e5ebb007d8d6b1941c4bb4893fc48c751

Fix compile error
Introduced in https://github.com/couchbase/couchbase-lite-android/commit/84b5842e0ffdd55b76452a0245452fd0e6f28608#diff-1

The exception type in the method signature of runOne() was changed.

src/androidTest/java/com/couchbase/lite/LitePerfTestCase.java
@@ -59,7 +59,7 @@ public abstract class LitePerfTestCase extends LiteTestCase {
         runMultiple();
     }
 
-    public double runOne(int numberOfDocuments, int sizeOfDocuments)  throws CouchbaseLiteException {
+    public double runOne(int numberOfDocuments, int sizeOfDocuments)  throws Exception {
         return 0;
     };
 