b12c2c53499c3df79a6ee0a508aa878f772533e3
Traun Leyden <tleyden@couchbase.com>
2014-10-22 11:20:32 -0700

    Default to DefaultSQLiteStorageEngineFactory return val
    https://github.com/couchbase/couchbase-lite-android/issues/431

--- src/main/java/com/couchbase/lite/android/AndroidSQLiteStorageEngineFactory.java
+++ src/main/java/com/couchbase/lite/android/AndroidSQLiteStorageEngineFactory.java
@@ -2,14 +2,23 @@ package com.couchbase.lite.android;
 
 import com.couchbase.lite.storage.SQLiteStorageEngine;
 import com.couchbase.lite.storage.SQLiteStorageEngineFactory;
+import com.couchbase.lite.storage.DefaultSQLiteStorageEngineFactory;
 
 /**
  * Android SQLiteStorageEngineFactory implementation
  */
 public class AndroidSQLiteStorageEngineFactory implements SQLiteStorageEngineFactory {
 
+    private SQLiteStorageEngineFactory _delegate = new DefaultSQLiteStorageEngineFactory();
+
     @Override
     public SQLiteStorageEngine createStorageEngine() {
-        return new AndroidSQLiteStorageEngine();
+
+        SQLiteStorageEngine storageEngine = _delegate.createStorageEngine();
+        if (storageEngine == null) {
+            return new AndroidSQLiteStorageEngine();
+        }
+        return storageEngine;
+        
     }
 }
