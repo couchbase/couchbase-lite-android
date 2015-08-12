8b0f5e24295a74f581c5fa7f0f9ed925907a0384
hideki <hideki@couchbase.com>
2015-08-11 17:46:10 -0700

    Fixed ARM v7a detection problem with Motorola Xoom

--- src/main/jni/util/com_couchbase_touchdb_TDCollateJSON_util.c
+++ src/main/jni/util/com_couchbase_touchdb_TDCollateJSON_util.c
@@ -1,16 +1,23 @@
 #include <jni.h>
 #include <cpu-features.h>
 
-jboolean
-Java_com_couchbase_touchdb_TDCollateJSON_isARMv7
-        (JNIEnv *env, jclass class) {
+
+#define DEBUG 0
+
+#if DEBUG
+#include <android/log.h>
+#  define  D(x...)  __android_log_print(ANDROID_LOG_INFO, "TDCollateJSON_isARMv7", x)
+#else
+#  define  D(...)  do {} while (0)
+#endif
+
+jboolean Java_com_couchbase_touchdb_TDCollateJSON_isARMv7(JNIEnv *env, jclass class) {
     uint64_t features = android_getCpuFeatures();
-    if ((android_getCpuFamily() != ANDROID_CPU_FAMILY_ARM) ||
-        ((features & ANDROID_CPU_ARM_FEATURE_ARMv7) == 0) ||
-        ((features & ANDROID_CPU_ARM_FEATURE_NEON) == 0)) {
-        return JNI_FALSE;
+    if (android_getCpuFamily() == ANDROID_CPU_FAMILY_ARM &&
+        (features & ANDROID_CPU_ARM_FEATURE_ARMv7) == ANDROID_CPU_ARM_FEATURE_ARMv7) {
+        return JNI_TRUE;
     }
     else {
-        return JNI_TRUE;
+        return JNI_FALSE;
     }
 }
