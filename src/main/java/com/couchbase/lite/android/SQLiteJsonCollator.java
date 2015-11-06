e32cf4e28a98d5e51c29882367ede9b55cc2f0e4
hideki <hideki@couchbase.com>
2015-11-05 18:14:21 -0800

    Cleanup SQLiteJsonCollator.java
    - compareStringsUnicode() method no longer will be used.

--- src/main/java/com/couchbase/lite/android/SQLiteJsonCollator.java
+++ src/main/java/com/couchbase/lite/android/SQLiteJsonCollator.java
@@ -1,18 +1,10 @@
 package com.couchbase.lite.android;
 
-import java.text.Collator;
-
 public class SQLiteJsonCollator {
     private static native void nativeRegister(Object database, String databaseClassName, int sdkVersion);
 
     public static void register(Object database, String databaseClassName, int sdkVersion) {
         nativeRegister(database, databaseClassName, sdkVersion);
-    }
-
-    public static int compareStringsUnicode(String a, String b) {
-        Collator c = Collator.getInstance();
-        int res = c.compare(a, b);
-        return res;
     }
 
     public static native int testCollateJSON(int mode, int len1, String string1, int len2, String string2);
