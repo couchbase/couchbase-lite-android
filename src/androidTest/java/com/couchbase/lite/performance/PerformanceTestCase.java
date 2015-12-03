8c7ec1a9b0a217f561aed5c16b6cf0f8e94697e6
Pasin Suriyentrakorn <pasin@couchbase.com>
2015-12-02 17:17:19 -0800

    Rename SQLite/SQLCipher native library package
    https://github.com/couchbase/couchbase-lite-java-core/issues/847

--- src/androidTest/java/com/couchbase/lite/performance/PerformanceTestCase.java
+++ src/androidTest/java/com/couchbase/lite/performance/PerformanceTestCase.java
@@ -80,7 +80,7 @@ public class PerformanceTestCase extends LiteTestCase {
     protected void setupSQLiteNativeLibrary() {
         int library = getSQLiteLibrary();
         if (library == 0)
-            SQLiteNativeLibrary.TEST_NATIVE_LIBRARY_NAME = SQLiteNativeLibrary.JNI_SQLITE_SYSTEM_LIBRARY;
+            SQLiteNativeLibrary.TEST_NATIVE_LIBRARY_NAME = SQLiteNativeLibrary.JNI_SQLITE_DEFAULT_LIBRARY;
         else if (library == 1)
             SQLiteNativeLibrary.TEST_NATIVE_LIBRARY_NAME = SQLiteNativeLibrary.JNI_SQLITE_CUSTOM_LIBRARY;
         else if (library == 2)
