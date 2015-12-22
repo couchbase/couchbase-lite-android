5a8cde1af07bbb79875778e606ced65b1a1e3144
hideki <hideki@couchbase.com>
2015-12-22 12:46:19 -0800

    fixed test

--- src/androidTest/java/com/couchbase/lite/performance/Test16_CompactDB.java
+++ src/androidTest/java/com/couchbase/lite/performance/Test16_CompactDB.java
@@ -5,7 +5,7 @@
  * except in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the
  * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language governing permissions
@@ -85,7 +85,7 @@ public class Test16_CompactDB extends PerformanceTestCase {
                         }
 
                         // deleting attachments
-                        if (deleteAttachment()) {
+                        if (attachment != null && deleteAttachment()) {
                             unsaved = document.createRevision();
                             for (int j = 0; j < getNumberOfAttacment(); j++) {
                                 String name = String.format("attach_%d", j);
