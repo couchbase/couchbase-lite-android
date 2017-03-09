cb9fcb9b9826e644a1c3b5fd26eebd789a60ce92
hideki <hideki@couchbase.com>
2017-03-09 15:51:22 -0800

    Added Logger

---
+++ android/CouchbaseLite/src/main/java/com/couchbase/lite/internal/support/LoggerImpl.java
@@ -0,0 +1,50 @@
+package com.couchbase.lite.internal.support;
+
+import android.util.Log;
+
+public class LoggerImpl implements Logger {
+    @Override
+    public void v(String tag, String msg) {
+        Log.v(tag, msg);
+    }
+
+    @Override
+    public void v(String tag, String msg, Throwable tr) {
+        Log.v(tag, msg, tr);
+    }
+
+    @Override
+    public void i(String tag, String msg) {
+        Log.i(tag, msg);
+    }
+
+    @Override
+    public void i(String tag, String msg, Throwable tr) {
+        Log.i(tag, msg, tr);
+    }
+
+    @Override
+    public void w(String tag, String msg) {
+        Log.w(tag, msg);
+    }
+
+    @Override
+    public void w(String tag, Throwable tr) {
+        Log.w(tag, tr);
+    }
+
+    @Override
+    public void w(String tag, String msg, Throwable tr) {
+        Log.w(tag, msg, tr);
+    }
+
+    @Override
+    public void e(String tag, String msg) {
+        Log.e(tag, msg);
+    }
+
+    @Override
+    public void e(String tag, String msg, Throwable tr) {
+        Log.e(tag, msg, tr);
+    }
+}
--- shared/src/main/java/com/couchbase/lite/Database.java
+++ shared/src/main/java/com/couchbase/lite/Database.java
@@ -1,10 +1,29 @@
 package com.couchbase.lite;
 
+import com.couchbase.lite.internal.bridge.LiteCoreBridge;
+import com.couchbase.lite.internal.support.Log;
+import com.couchbase.litecore.LiteCoreException;
+
 import java.io.File;
 
 public final class Database {
+    //---------------------------------------------
+    // static variables
+    //---------------------------------------------
+    private static final String DB_EXTENSION = "cblite2";
+
+    //---------------------------------------------
+    // member variables
+    //---------------------------------------------
     private String name;
     private String path;
+    private DatabaseOptions options;
+    // TODO: class name is conflicting between API level and LiteCore
+    private com.couchbase.litecore.Database db;
+
+    //---------------------------------------------
+    // API - public methods
+    //---------------------------------------------
 
     public Database(String name) throws CouchbaseLiteException {
         this.name = name;
@@ -70,4 +89,65 @@ public final class Database {
     // TODO: Notification will be implemented in DB4
     // func addChangeListener(docListener: DocumentChangeListener)
     // func removeChangeListener(docListener: DocumentChangeListener)
+
+
+    //---------------------------------------------
+    // Package level access
+    //---------------------------------------------
+
+    //---------------------------------------------
+    // Private (in class only)
+    //---------------------------------------------
+
+    private boolean open() {
+        if (db != null) return true;
+
+        File dir = options.getDirectory() != null ? options.getDirectory() : getDefaultDirectory();
+        if (setupDirectory(dir))
+            return false;
+
+        File dbFile = getDatabasePath(dir, name);
+
+        // databaseFlags
+        int databaseFlags;
+        if (options.isReadOnly())
+            databaseFlags = com.couchbase.litecore.Database.ReadOnly;
+        else
+            databaseFlags = com.couchbase.litecore.Database.Create;
+
+        // TODO: encryptionAlgorithm, encryptionKey
+        int encryptionAlgorithm = com.couchbase.litecore.Database.NoEncryption;
+        byte[] encryptionKey = null;
+
+        Log.i(Log.DATABASE, "Opening %s at path %s", this, dbFile.getPath());
+
+        try {
+            // TODO: com.couchbase.litecore.Database is same class name with this classname.
+            //       Need to change the name.
+            com.couchbase.litecore.Database db = new com.couchbase.litecore.Database(
+                    dbFile.getPath(),
+                    databaseFlags,
+                    encryptionAlgorithm,
+                    encryptionKey);
+        } catch (LiteCoreException ex) {
+            throw LiteCoreBridge.convertException(ex);
+        }
+
+        // TODO: Other settings
+
+        return true;
+    }
+
+    private File getDefaultDirectory() {
+        return null;
+    }
+
+    private boolean setupDirectory(File dir) {
+        return true;
+    }
+
+    private static File getDatabasePath(File dir, String name){
+        name = name.replaceAll("/",":"); // TODO: This does not work with Windows platform.
+        return new File(dir, name);
+    }
 }
