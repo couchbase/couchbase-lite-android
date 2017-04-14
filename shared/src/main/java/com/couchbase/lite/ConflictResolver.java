5fe7ebc7f327a3e835efea58fdaefa8fe2207eb7
hideki <hideki@couchbase.com>
2017-04-14 13:23:40 -0700

    Updated the  documentation of ConflictResolver.

--- shared/src/main/java/com/couchbase/lite/ConflictResolver.java
+++ shared/src/main/java/com/couchbase/lite/ConflictResolver.java
@@ -16,8 +16,23 @@ package com.couchbase.lite;
 import java.util.Map;
 
 /**
- * TODO: DB00x
+ * Interface for an application-defined object that can resolve a conflict between two revisions of
+ * a document. Called when saving a Document, when there is a newer revision already in the database;
+ * and also when the replicator pulls a remote revision that conflicts with a locally-saved revision.
  */
 public interface ConflictResolver {
-    Map<String, Object> resolve(Map<String, Object> mine, Map<String, Object> theirs, Map<String, Object> base);
+    /**
+     * Resolves conflicting edits of a document against their common base.
+     *
+     * @param localProperties       The revision that is being saved, or the revision in the local
+     *                              database for which there is a server side conflict.
+     * @param conflictingProperties The conflicting revision that is already stored in the database,
+     *                              or on the server.
+     * @param baseProperties        The common parent revision of these two revisions, if available.
+     * @return the resolved set of properties for the document to store, or null to give up if
+     * automatic resolution isn't possible.
+     */
+    Map<String, Object> resolve(Map<String, Object> localProperties,
+                                Map<String, Object> conflictingProperties,
+                                Map<String, Object> baseProperties);
 }
