b69047fc916dbf24455db932f4551b621b37b9e7
hideki <hideki@couchbase.com>
2017-09-15 17:17:02 -0700

    Fixed #1286 - 2.0 Attachments in auto upgraded DB from 1.x is not accessible through getBlob()

--- shared/src/main/java/com/couchbase/lite/CBLData.java
+++ shared/src/main/java/com/couchbase/lite/CBLData.java
@@ -79,9 +79,7 @@ class CBLData {
                 if (value.getType() != kFLDict)
                     throw new IllegalStateException("value is not kFLDict");
                 FLDict flDict = value.asFLDict();
-                FLValue flType = SharedKeys.getValue(flDict, Blob.kC4ObjectTypeProperty, database.getSharedKeys());
-                String type = flType != null ? flType.asString() : null;
-                if (type == null) {
+                if (!isBlob(flDict, database) && !isOldAttachment(flDict, database)) {
                     return new Dictionary(new CBLFLDict(flDict, c4doc, database));
                 } else { // type == "blob"
                     Object result = SharedKeys.valueToObject(value, database.getSharedKeys());
@@ -106,9 +104,7 @@ class CBLData {
                 if (value.getType() != kFLDict)
                     throw new IllegalStateException("value is not kFLDict");
                 FLDict flDict = value.asFLDict();
-                FLValue flType = getValue(flDict, Blob.kC4ObjectTypeProperty, database.getSharedKeys());
-                String type = flType != null ? flType.asString() : null;
-                if (type == null) {
+                if (!isBlob(flDict, database) && !isOldAttachment(flDict, database)) {
                     return new Dictionary(new CBLFLDict(flDict, flDataSource, database));
                 } else { // type == "blob"
                     Object result = SharedKeys.valueToObject(value, database.getSharedKeys());
@@ -122,10 +118,43 @@ class CBLData {
     }
 
     private static Object dictionaryToCBLObject(Map<String, Object> dict, Database database) {
-        String type = (String) dict.get(Blob.kC4ObjectTypeProperty);
-        if (type != null && type.equals(Blob.kC4ObjectType_Blob)) {
+        if (isBlob(dict) || isOldAttachment(dict)) {
             return new Blob(database, dict);
         }
         return null;
     }
+
+    private static boolean isBlob(FLDict flDict, Database database) {
+        FLValue flType = getValue(flDict, Blob.kC4ObjectTypeProperty, database.getSharedKeys());
+        String type = flType != null ? flType.asString() : null;
+        return type != null && type.equals(Blob.kC4ObjectType_Blob);
+    }
+
+    private static boolean isBlob(Map<String, Object> dict) {
+        String type = (String) dict.get(Blob.kC4ObjectTypeProperty);
+        return type != null && type.equals(Blob.kC4ObjectType_Blob);
+    }
+
+    // NOTE: CBL v1.x does not add `"@type": "blob"` entry in the dictionary. Following is
+    // a workaround to detect CBL v1.x attachment.
+
+    private static boolean isOldAttachment(FLDict flDict, Database database) {
+        FLValue flDigest = getValue(flDict, "digest", database.getSharedKeys());
+        FLValue flLength = getValue(flDict, "length", database.getSharedKeys());
+        FLValue flStub = getValue(flDict, "stub", database.getSharedKeys());
+        FLValue flRevPos = getValue(flDict, "revpos", database.getSharedKeys());
+        FLValue flContentType = getValue(flDict, "content_type", database.getSharedKeys());
+        return flDigest != null && flLength != null && flStub != null && flRevPos != null && flContentType != null;
+    }
+
+    private static boolean isOldAttachment(Map<String, Object> dict) {
+        Object digest = dict.get("digest");
+        Object length = dict.get("length");
+        Object stub = dict.get("stub");
+        Object revpos = dict.get("revpos");
+        Object contentType = dict.get("content_type");
+        return digest != null && length != null && stub != null && revpos != null && contentType != null;
+    }
+
+
 }
