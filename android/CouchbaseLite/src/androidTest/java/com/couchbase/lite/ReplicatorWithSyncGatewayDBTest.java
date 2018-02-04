27a5d6a9500025b94341c417dc0185b4069d38b8
Hideki Itakura <hideki@couchbase.com>
2018-02-03 22:22:38 -0800

    Fixed #1575 - update copyright (#1579)

--- android/CouchbaseLite/src/androidTest/java/com/couchbase/lite/ReplicatorWithSyncGatewayDBTest.java
+++ android/CouchbaseLite/src/androidTest/java/com/couchbase/lite/ReplicatorWithSyncGatewayDBTest.java
@@ -1,3 +1,20 @@
+//
+// ReplicatorWithSyncGatewayDBTest.java
+//
+// Copyright (c) 2017 Couchbase, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 package com.couchbase.lite;
 
 import com.couchbase.lite.internal.support.Log;
@@ -35,7 +52,6 @@ public class ReplicatorWithSyncGatewayDBTest extends BaseReplicatorTest {
         if (config.replicatorTestsEnabled()) {
             remote_PUT_db(DB_NAME);
         }
-
     }
 
     @After
