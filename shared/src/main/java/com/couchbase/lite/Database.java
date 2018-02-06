eaf5bbc04e7ee83f4049015a2d8f17162842eb67
Hideki Itakura <hideki@couchbase.com>
2018-02-05 16:46:39 -0800

    Fixed #1581 - 2.0 : LiveQuery threading issue (#1585)

--- shared/src/main/java/com/couchbase/lite/Database.java
+++ shared/src/main/java/com/couchbase/lite/Database.java
@@ -1282,26 +1282,6 @@ public final class Database {
         }
     }
 
-    /*
-    private void shutdownAndAwaitTermination(ExecutorService pool, int waitSec) {
-        pool.shutdown(); // Disable new tasks from being submitted
-        try {
-            // Wait a while for existing tasks to terminate
-            if (!pool.awaitTermination(waitSec, TimeUnit.SECONDS)) {
-                pool.shutdownNow(); // Cancel currently executing tasks
-                // Wait a while for tasks to respond to being cancelled
-                if (!pool.awaitTermination(waitSec, TimeUnit.SECONDS))
-                    Log.w(TAG, "Pool did not terminate");
-            }
-        } catch (InterruptedException ie) {
-            // (Re-)Cancel if current thread also interrupted
-            pool.shutdownNow();
-            // Preserve interrupt status
-            Thread.currentThread().interrupt();
-        }
-    }
-    */
-
     private void stopAllActiveReplicatoin() {
         // stop replicator
         synchronized (activeReplications) {
