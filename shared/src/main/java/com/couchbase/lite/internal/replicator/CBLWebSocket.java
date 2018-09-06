1690a58f8c4ba80152ccb1ab49910c528232f927
Pasin Suriyentrakorn <pasin@couchbase.com>
2018-09-05 21:31:51 -0700

    Enable TLSv1.1 and 1.2 for Android API 19
    * Implemented TLSSocketFactory that explictly enables TLSv1, TSLv1.2, and TLSv1.2
    * Refactored the code to always call setupSSLSocketFactory() to setup the SSLSocketFactory.
    
    #1756

--- shared/src/main/java/com/couchbase/lite/internal/replicator/CBLWebSocket.java
+++ shared/src/main/java/com/couchbase/lite/internal/replicator/CBLWebSocket.java
@@ -25,10 +25,14 @@ import com.couchbase.litecore.fleece.FLValue;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
+import java.security.SecureRandom;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.util.Arrays;
@@ -39,8 +43,10 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
@@ -175,10 +181,6 @@ public final class CBLWebSocket extends C4Socket {
     // Abstract method implementation
     //-------------------------------------------------------------------------
 
-    // TODO !!!!!
-    //@Override
-
-
     @Override
     protected void send(byte[] allocatedData) {
         if (this.webSocket.send(ByteString.of(allocatedData, 0, allocatedData.length)))
@@ -260,8 +262,8 @@ public final class CBLWebSocket extends C4Socket {
         if (authenticator != null)
             builder.authenticator(authenticator);
 
-        // trusted certificate (pinned certificate)
-        setupTrustedCertificate(builder);
+        // setup SSLFactory and trusted certificate (pinned certificate)
+        setupSSLSocketFactory(builder);
 
         return builder.build();
     }
@@ -311,23 +313,6 @@ public final class CBLWebSocket extends C4Socket {
         return null;
     }
 
-    private void setupTrustedCertificate(OkHttpClient.Builder builder) throws GeneralSecurityException {
-        if (options != null && options.containsKey(kC4ReplicatorOptionPinnedServerCert)) {
-            byte[] pin = (byte[]) options.get(kC4ReplicatorOptionPinnedServerCert);
-            if (pin != null) {
-                X509TrustManager trustManager = trustManagerForCertificates(toStream(pin));
-                SSLContext sslContext = SSLContext.getInstance("TLS");
-                sslContext.init(null, new TrustManager[]{trustManager}, null);
-                SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-                if (trustManager != null && sslSocketFactory != null)
-                    builder.sslSocketFactory(sslSocketFactory, trustManager);
-
-                // custom hostname verifier - allow IP address and empty Common Name (CN).
-                builder.hostnameVerifier(CustomHostnameVerifier.getInstance());
-            }
-        }
-    }
-
     private InputStream toStream(byte[] pin) {
         return new Buffer().write(pin).inputStream();
     }
@@ -339,7 +324,6 @@ public final class CBLWebSocket extends C4Socket {
         }
         return result;
     }
-
 
     private Request newRequest() {
         Request.Builder builder = new Request.Builder();
@@ -404,50 +388,6 @@ public final class CBLWebSocket extends C4Socket {
         }
     }
 
-    // https://github.com/square/okhttp/wiki/HTTPS
-    // https://github.com/square/okhttp/blob/master/samples/guide/src/main/java/okhttp3/recipes/CustomTrust.java
-    private X509TrustManager trustManagerForCertificates(InputStream in) throws GeneralSecurityException {
-        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-        Collection<? extends Certificate> certificates = certificateFactory.generateCertificates(in);
-        if (certificates.isEmpty()) {
-            throw new IllegalArgumentException("expected non-empty set of trusted certificates");
-        }
-
-        // Put the certificates a key store.
-        char[] password = "umwxnikwxx".toCharArray(); // Any password will work.
-        KeyStore keyStore = newEmptyKeyStore(password);
-        int index = 0;
-        for (Certificate certificate : certificates) {
-            String certificateAlias = Integer.toString(index++);
-            keyStore.setCertificateEntry(certificateAlias, certificate);
-        }
-
-        // Use it to build an X509 trust manager.
-        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(
-                KeyManagerFactory.getDefaultAlgorithm());
-        keyManagerFactory.init(keyStore, password);
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
-                TrustManagerFactory.getDefaultAlgorithm());
-        trustManagerFactory.init(keyStore);
-        TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-        if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
-            throw new IllegalStateException("Unexpected default trust managers:"
-                    + Arrays.toString(trustManagers));
-        }
-        return (X509TrustManager) trustManagers[0];
-    }
-
-    private KeyStore newEmptyKeyStore(char[] password) throws GeneralSecurityException {
-        try {
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            InputStream in = null; // By convention, 'null' creates an empty key store.
-            keyStore.load(in, password);
-            return keyStore;
-        } catch (IOException e) {
-            throw new AssertionError(e);
-        }
-    }
-
     private void didClose(int code, String reason) {
         if (code == kWebSocketCloseNormal) {
             didClose(null);
@@ -508,5 +448,146 @@ public final class CBLWebSocket extends C4Socket {
         else if (scheme.equalsIgnoreCase(kC4Replicator2TLSScheme))
             return WEBSOCKET_SECURE_CONNECTION_SCHEME;
         return scheme;
+    }
+
+    //-------------------------------------------------------------------------
+    // SSL Support
+    //-------------------------------------------------------------------------
+
+    private void setupSSLSocketFactory(OkHttpClient.Builder builder) throws GeneralSecurityException {
+        boolean isPinningServerCert = false;
+        X509TrustManager trustManager = null;
+        if (options != null && options.containsKey(kC4ReplicatorOptionPinnedServerCert)) {
+            byte[] pin = (byte[]) options.get(kC4ReplicatorOptionPinnedServerCert);
+            if (pin != null) {
+                trustManager = trustManagerForCertificates(toStream(pin));
+                isPinningServerCert = true;
+            }
+        }
+
+        if (trustManager == null)
+            trustManager = defaultTrustManager();
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, new TrustManager[]{ trustManager }, null);
+        SSLSocketFactory sslSocketFactory = new TLSSocketFactory(null,
+                new TrustManager[]{ trustManager }, null);
+        builder.sslSocketFactory(sslSocketFactory, trustManager);
+
+        if (isPinningServerCert) {
+            // Custom hostname verifier - allow IP address and empty Common Name (CN).
+            builder.hostnameVerifier(CustomHostnameVerifier.getInstance());
+        }
+    }
+
+    private X509TrustManager defaultTrustManager() throws GeneralSecurityException {
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init((KeyStore) null);
+        TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+        if (trustManagers.length == 0)
+            throw new IllegalStateException("Cannot find the default trust manager");
+        return (X509TrustManager) trustManagers[0];
+    }
+
+    // https://github.com/square/okhttp/wiki/HTTPS
+    // https://github.com/square/okhttp/blob/master/samples/guide/src/main/java/okhttp3/recipes/CustomTrust.java
+    private X509TrustManager trustManagerForCertificates(InputStream in) throws GeneralSecurityException {
+        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+        Collection<? extends Certificate> certificates = certificateFactory.generateCertificates(in);
+        if (certificates.isEmpty()) {
+            throw new IllegalArgumentException("expected non-empty set of trusted certificates");
+        }
+
+        // Put the certificates a key store.
+        char[] password = "umwxnikwxx".toCharArray(); // Any password will work.
+        KeyStore keyStore = newEmptyKeyStore(password);
+        int index = 0;
+        for (Certificate certificate : certificates) {
+            String certificateAlias = Integer.toString(index++);
+            keyStore.setCertificateEntry(certificateAlias, certificate);
+        }
+
+        // Use it to build an X509 trust manager.
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(
+                KeyManagerFactory.getDefaultAlgorithm());
+        keyManagerFactory.init(keyStore, password);
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(keyStore);
+        TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+        if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+            throw new IllegalStateException("Unexpected default trust managers:"
+                    + Arrays.toString(trustManagers));
+        }
+        return (X509TrustManager) trustManagers[0];
+    }
+
+    private KeyStore newEmptyKeyStore(char[] password) throws GeneralSecurityException {
+        try {
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            InputStream in = null; // By convention, 'null' creates an empty key store.
+            keyStore.load(in, password);
+            return keyStore;
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    /**
+     * Workaround to enable both TLS1.1 and TLS1.2 for Android API 16 - 19.
+     * When starting to support from API 20, we could remove the workaround.
+     */
+    private static class TLSSocketFactory extends SSLSocketFactory {
+        private SSLSocketFactory delegate;
+        public TLSSocketFactory(KeyManager[] keyManagers,
+                                TrustManager[] trustManagers,
+                                SecureRandom secureRandom)
+                throws GeneralSecurityException {
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(keyManagers, trustManagers, secureRandom);
+            delegate = context.getSocketFactory();
+        }
+
+        @Override
+        public String[] getDefaultCipherSuites() {
+            return delegate.getDefaultCipherSuites();
+        }
+
+        @Override
+        public String[] getSupportedCipherSuites() {
+            return delegate.getSupportedCipherSuites();
+        }
+
+        @Override
+        public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException {
+            return setEnabledProtocols(delegate.createSocket(socket, host, port, autoClose));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port) throws IOException, UnknownHostException {
+            return setEnabledProtocols(delegate.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port, InetAddress localHost, int localPort) throws IOException, UnknownHostException {
+            return setEnabledProtocols(delegate.createSocket(host, port, localHost, localPort));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress address, int port) throws IOException {
+            return setEnabledProtocols(delegate.createSocket(address, port));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress inetAddress, int port, InetAddress localAddress, int localPort) throws IOException {
+            return setEnabledProtocols(delegate.createSocket(inetAddress, port, localAddress, localPort));
+        }
+
+        private Socket setEnabledProtocols(Socket socket) {
+            if(socket != null && (socket instanceof SSLSocket)) {
+                ((SSLSocket)socket).setEnabledProtocols(new String[] {"TLSv1", "TLSv1.1", "TLSv1.2"});
+            }
+            return socket;
+        }
     }
 }
