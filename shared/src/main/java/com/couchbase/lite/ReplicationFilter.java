ad3b84f35ce2676d16105dc2f29974fa50ca566d
Sandychuang8 <sandy.chuang@couchbase.com>
2018-12-15 19:51:44 -0800

    Replication filter
    Replication filter: Add comments for replicator interface interface.

--- shared/src/main/java/com/couchbase/lite/ReplicationFilter.java
+++ shared/src/main/java/com/couchbase/lite/ReplicationFilter.java
@@ -17,6 +17,11 @@
 //
 package com.couchbase.lite;
 
+/**
+ * Interface delegate that takes Document input parameter and bool output parameter
+ * Document push and pull will be allowed if output is true, othewise, Document 
+ * push and pull will not be allowed.
+ **/
 public interface ReplicationFilter {
     boolean filtered(Document document, boolean isDeleted);
 }
