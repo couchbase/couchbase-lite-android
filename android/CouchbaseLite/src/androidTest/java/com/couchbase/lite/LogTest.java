e749f9325c84a9afaea018a1cb4efc5edf1ca20f
jayahariv <jayahariv88@gmail.com>
2019-01-23 12:12:38 -0800

    test: add file logging tests
    * add regions for folding the code
    * check file logging in binary format as well as usePlainText
    * check file logging file name

--- android/CouchbaseLite/src/androidTest/java/com/couchbase/lite/LogTest.java
+++ android/CouchbaseLite/src/androidTest/java/com/couchbase/lite/LogTest.java
@@ -10,11 +10,17 @@ import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.IOException;
+import java.io.FilenameFilter;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class LogTest {
@@ -25,6 +31,7 @@ public class LogTest {
         context = InstrumentationRegistry.getTargetContext();
     }
 
+    //region Custom Logging Tests
     @Test
     public void testCustomLoggingLevels() {
         LogTestLogger customLogger = new LogTestLogger();
@@ -43,8 +50,34 @@ public class LogTest {
     }
 
     @Test
-    public void testPlaintextLoggingLevels() throws IOException {
-        final File path = new File(context.getCacheDir().getAbsolutePath(), "testPlaintextLoggingLevels");
+    public void testEnableAndDisableCustomLogging() {
+        LogTestLogger customLogger = new LogTestLogger();
+        Log.i("IGNORE", "IGNORE");
+        Database.log.setCustom(customLogger);
+
+        customLogger.setLevel(LogLevel.NONE);
+        Log.v(LogDomain.DATABASE.toString(), "TEST VERBOSE");
+        Log.i(LogDomain.DATABASE.toString(), "TEST INFO");
+        Log.w(LogDomain.DATABASE.toString(), "TEST WARNING");
+        Log.e(LogDomain.DATABASE.toString(), "TEST ERROR");
+        assertEquals(0, customLogger.getLines().size());
+
+        customLogger.setLevel(LogLevel.VERBOSE);
+        Log.v(LogDomain.DATABASE.toString(), "TEST VERBOSE");
+        Log.i(LogDomain.DATABASE.toString(), "TEST INFO");
+        Log.w(LogDomain.DATABASE.toString(), "TEST WARNING");
+        Log.e(LogDomain.DATABASE.toString(), "TEST ERROR");
+        assertEquals(4, customLogger.getLines().size());
+    }
+    //endregion
+
+    //region File Logging Tests
+    @Test
+    public void testFileLoggingLoggingLevels() {
+        final File path = new File(
+                context.getCacheDir().getAbsolutePath(),
+                "testFileLoggingLoggingLevels"
+        );
         final String logDirectory = emptyDirectory(path.getAbsolutePath());
         LogFileConfiguration config = new LogFileConfiguration(logDirectory)
                 .setUsePlaintext(true)
@@ -53,7 +86,13 @@ public class LogTest {
         testWithConfiguration(LogLevel.INFO, config, new Runnable() {
             @Override
             public void run() {
-                LogLevel levels[] = { LogLevel.NONE, LogLevel.ERROR, LogLevel.WARNING, LogLevel.INFO, LogLevel.VERBOSE };
+                LogLevel levels[] = {
+                        LogLevel.NONE,
+                        LogLevel.ERROR,
+                        LogLevel.WARNING,
+                        LogLevel.INFO,
+                        LogLevel.VERBOSE
+                };
                 for (LogLevel level : levels) {
                     Database.log.getFile().setLevel(level);
                     Log.v("DATABASE", "TEST VERBOSE");
@@ -86,31 +125,117 @@ public class LogTest {
                 }
             }
         });
-
-
     }
 
     @Test
-    public void testEnableAndDisableCustomLogging() {
-        LogTestLogger customLogger = new LogTestLogger();
-        Log.i("IGNORE", "IGNORE");
-        Database.log.setCustom(customLogger);
+    public void testFileLoggingDefaultBinaryFormat() {
+        final File path = new File(
+                context.getCacheDir().getAbsolutePath(),
+                "testFileLoggingDefaultBinaryFormat"
+        );
+        final String logDirectory = emptyDirectory(path.getAbsolutePath());
+        LogFileConfiguration config = new LogFileConfiguration(logDirectory);
 
-        customLogger.setLevel(LogLevel.NONE);
-        Log.v(LogDomain.DATABASE.toString(), "TEST VERBOSE");
-        Log.i(LogDomain.DATABASE.toString(), "TEST INFO");
-        Log.w(LogDomain.DATABASE.toString(), "TEST WARNING");
-        Log.e(LogDomain.DATABASE.toString(), "TEST ERROR");
-        assertEquals(0, customLogger.getLines().size());
+        testWithConfiguration(LogLevel.INFO, config, new Runnable() {
+            @Override
+            public void run() {
+                Log.i("DATABASE", "TEST INFO");
 
-        customLogger.setLevel(LogLevel.VERBOSE);
-        Log.v(LogDomain.DATABASE.toString(), "TEST VERBOSE");
-        Log.i(LogDomain.DATABASE.toString(), "TEST INFO");
-        Log.w(LogDomain.DATABASE.toString(), "TEST WARNING");
-        Log.e(LogDomain.DATABASE.toString(), "TEST ERROR");
-        assertEquals(4, customLogger.getLines().size());
+                File[] files = path.listFiles();
+                assertTrue(files != null);
+                assertTrue(files.length > 0);
+                File lastModifiedFile = files[0];
+                for(File log : files) {
+                    if (log.lastModified() > lastModifiedFile.lastModified()) {
+                        lastModifiedFile = log;
+                    }
+                }
+
+                try {
+                    byte[] bytes = new byte[4];
+                    InputStream is = new FileInputStream(lastModifiedFile);
+                    assertTrue(is.read(bytes) == 4);
+                    assertTrue(bytes[0] == (byte) 0xCF);
+                    assertTrue(bytes[1] == (byte) 0xB2);
+                    assertTrue(bytes[2] == (byte) 0xAB);
+                    assertTrue(bytes[3] == (byte) 0x1B);
+                } catch(Exception e) {
+                    fail("Exception during test callback " + e.toString());
+                }
+            }
+        });
     }
 
+    @Test
+    public void testFileLoggingUsePlainText() {
+        final File path = new File(
+                context.getCacheDir().getAbsolutePath(),
+                "testFileLoggingUsePlainText"
+        );
+        final String logDirectory = emptyDirectory(path.getAbsolutePath());
+        LogFileConfiguration config = new LogFileConfiguration(logDirectory).setUsePlaintext(true);
+
+        testWithConfiguration(LogLevel.INFO, config, new Runnable() {
+            @Override
+            public void run() {
+                String uuidString = UUID.randomUUID().toString();
+                Log.i(LogDomain.DATABASE.toString(), uuidString);
+
+                File[] files = path.listFiles(new FilenameFilter() {
+                    @Override
+                    public boolean accept(File dir, String name) {
+                        return name.toLowerCase().startsWith("cbl_info_");
+                    }
+                });
+                assertTrue(files != null);
+                assertTrue(files.length > 0);
+
+                File lastModifiedFile = files[0];
+                for(File log : files) {
+                    if (log.lastModified() > lastModifiedFile.lastModified()) {
+                        lastModifiedFile = log;
+                    }
+                }
+
+                try {
+                    byte[] encoded = Files.readAllBytes(lastModifiedFile.toPath());
+                    String contentsOfLastModified = new String(encoded, StandardCharsets.US_ASCII);
+                    assertTrue(contentsOfLastModified.contains(uuidString));
+
+                } catch (Exception e) {
+                    fail("Exception during test callback " + e.toString());
+                }
+            }
+        });
+    }
+
+    @Test
+    public void testFileLoggingLogFilename() {
+        final File path = new File(
+                context.getCacheDir().getAbsolutePath(),
+                "testFileLoggingLogFilename"
+        );
+        final String logDirectory = emptyDirectory(path.getAbsolutePath());
+        LogFileConfiguration config = new LogFileConfiguration(logDirectory);
+
+        testWithConfiguration(LogLevel.DEBUG, config, new Runnable() {
+            @Override
+            public void run() {
+                Log.i(LogDomain.DATABASE.toString(), "TEST MESSAGE");
+                File[] files = path.listFiles();
+                assertTrue(files != null);
+                assertTrue(files.length > 0);
+
+                String filenameRegex = "cbl_(debug|verbose|info|warning|error)_\\d+\\.cbllog";
+                for (File file : files) {
+                    assertTrue(file.getName().matches(filenameRegex));
+                }
+            }
+        });
+    }
+    //endregion
+
+    //region Helper methods
     private void testWithConfiguration(LogLevel level, LogFileConfiguration config, Runnable r) {
         LogFileConfiguration old = Database.log.getFile().getConfig();
         Database.log.getFile().setConfig(config);
@@ -133,6 +258,7 @@ public class LogTest {
 
         return path;
     }
+    //endregion
 }
 
 class LogTestLogger implements Logger {
