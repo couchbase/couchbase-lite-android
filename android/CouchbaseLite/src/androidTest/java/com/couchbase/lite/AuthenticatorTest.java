9ceb852a69e2f666d52dae3de5abd00044098fa4
jayahariv <jayahariv88@gmail.com>
2019-02-05 13:00:08 -0800

    test: add authenticator test
    * add authenticator test for basic and session authenticator

---
+++ android/CouchbaseLite/src/androidTest/java/com/couchbase/lite/AuthenticatorTest.java
@@ -0,0 +1,35 @@
+package com.couchbase.lite;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class AuthenticatorTest extends BaseTest {
+
+    @Test
+    public void testBasicAuthenticatorInstance() throws CouchbaseLiteException {
+        String username = "someUsername";
+        String password = "somePassword";
+        BasicAuthenticator auth = new BasicAuthenticator(username, password);
+        assertEquals(auth.getUsername(), username);
+        assertEquals(auth.getPassword(), password);
+    }
+
+    @Test
+    public void testSessionAuthenticatorWithSessionID() throws CouchbaseLiteException {
+        String sessionID = "someSessionID";
+        SessionAuthenticator auth = new SessionAuthenticator(sessionID);
+        assertEquals(auth.getSessionID(), sessionID);
+        assertEquals(auth.getCookieName(), "SyncGatewaySession");
+    }
+
+    @Test
+    public void testSessionAuthenticatorWithSessionIDAndCookie() throws CouchbaseLiteException {
+        String sessionID = "someSessionID";
+        String cookie = "someCookie";
+        SessionAuthenticator auth = new SessionAuthenticator(sessionID, cookie);
+        assertEquals(auth.getSessionID(), sessionID);
+        assertEquals(auth.getCookieName(), cookie);
+    }
+}
