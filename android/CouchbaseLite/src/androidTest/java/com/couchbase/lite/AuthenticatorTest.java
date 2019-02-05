96dcab78694f654905856e0d5e7672b4a190a3a3
jayahariv <jayahariv88@gmail.com>
2019-02-05 13:02:00 -0800

    fix: remove unnecessary import

--- android/CouchbaseLite/src/androidTest/java/com/couchbase/lite/AuthenticatorTest.java
+++ android/CouchbaseLite/src/androidTest/java/com/couchbase/lite/AuthenticatorTest.java
@@ -3,7 +3,6 @@ package com.couchbase.lite;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 public class AuthenticatorTest extends BaseTest {
 
