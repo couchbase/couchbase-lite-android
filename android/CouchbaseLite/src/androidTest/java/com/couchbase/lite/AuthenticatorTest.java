a75efbb6b1aa60723242f65709b463b514917b59
Jayahari Vavachan <jayahariv88@gmail.com>
2019-02-05 21:47:43 -0800

    test: add authenticator test (#1855)
    * add authenticator test for basic and session authenticator
    * add empty session ID test
    * add empty cookie test

---
+++ android/CouchbaseLite/src/androidTest/java/com/couchbase/lite/AuthenticatorTest.java
@@ -0,0 +1,53 @@
+package com.couchbase.lite;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
+public class AuthenticatorTest extends BaseTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testBasicAuthenticatorInstance() throws CouchbaseLiteException {
+        String username = "someUsername";
+        String password = "somePassword";
+        BasicAuthenticator auth = new BasicAuthenticator(username, password);
+        assertEquals(auth.getUsername(), username);
+        assertEquals(auth.getPassword(), password);
+    }
+
+    @Test
+    public void testSessionAuthenticatorWithSessionID() throws CouchbaseLiteException {
+        String sessionID = "someSessionID";
+        SessionAuthenticator auth = new SessionAuthenticator(sessionID);
+        assertEquals(auth.getSessionID(), sessionID);
+        assertEquals(auth.getCookieName(), "SyncGatewaySession");
+    }
+
+    @Test
+    public void testSessionAuthenticatorWithSessionIDAndCookie() throws CouchbaseLiteException {
+        String sessionID = "someSessionID";
+        String cookie = "someCookie";
+        SessionAuthenticator auth = new SessionAuthenticator(sessionID, cookie);
+        assertEquals(auth.getSessionID(), sessionID);
+        assertEquals(auth.getCookieName(), cookie);
+    }
+
+    @Test
+    public void testSessionAuthenticatorEmptySessionID() throws CouchbaseLiteException {
+        thrown.expect(IllegalArgumentException.class);
+        SessionAuthenticator auth = new SessionAuthenticator(null, null);
+    }
+
+    @Test
+    public void testSessionAuthenticatorEmptyCookie() throws CouchbaseLiteException {
+        String sessionID = "someSessionID";
+        SessionAuthenticator auth = new SessionAuthenticator(sessionID, null);
+        assertEquals(auth.getSessionID(), sessionID);
+        assertEquals(auth.getCookieName(), "SyncGatewaySession");
+    }
+}
