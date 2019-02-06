66766c2437c6ee4078873d3197196734b5043fe1
jayahariv <jayahariv88@gmail.com>
2019-02-05 21:16:59 -0800

    fix: rename the test class
    * renamed to ReplicatorMiscTest instead of ReplicatorMisc

--- android/CouchbaseLite/src/androidTest/java/com/couchbase/lite/ReplicatorMiscTest.java
+++ android/CouchbaseLite/src/androidTest/java/com/couchbase/lite/ReplicatorMiscTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class ReplicatorMisc extends BaseReplicatorTest {
+public class ReplicatorMiscTest extends BaseReplicatorTest {
 
     @Test
     public void testReplicatorChange() {
