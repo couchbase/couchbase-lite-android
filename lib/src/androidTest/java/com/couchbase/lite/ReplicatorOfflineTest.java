7bb744d464deeb3264737c957e77e8b93a4fff8a
G. Blake Meike <blake.meike@couchbase.com>
2019-05-08 16:15:56 -0700

    reformat test code

--- lib/src/androidTest/java/com/couchbase/lite/ReplicatorOfflineTest.java
+++ lib/src/androidTest/java/com/couchbase/lite/ReplicatorOfflineTest.java
@@ -1,28 +1,22 @@
 package com.couchbase.lite;
 
-import android.support.annotation.NonNull;
-
-import org.junit.Test;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
 
 public class ReplicatorOfflineTest extends BaseReplicatorTest {
 
     @Test
     public void testEditReadOnlyConfiguration() throws Exception {
         Endpoint endpoint = getRemoteTargetEndpoint();
-        ReplicatorConfiguration config = makeConfig(
-                true,
-                false,
-                true,
-                endpoint
-        );
+        ReplicatorConfiguration config = makeConfig(true, false, true, endpoint);
         config.setContinuous(false);
         repl = new Replicator(config);
 
@@ -37,19 +31,16 @@ public class ReplicatorOfflineTest extends BaseReplicatorTest {
         Replicator repl = new Replicator(config);
         final CountDownLatch offline = new CountDownLatch(1);
         final CountDownLatch stopped = new CountDownLatch(1);
-        ListenerToken token = repl.addChangeListener(executor, new ReplicatorChangeListener() {
-            @Override
-            public void changed(ReplicatorChange change) {
+        ListenerToken token = repl.addChangeListener(
+            executor,
+            change -> {
                 Replicator.Status status = change.getStatus();
                 if (status.getActivityLevel() == Replicator.ActivityLevel.OFFLINE) {
                     change.getReplicator().stop();
                     offline.countDown();
                 }
-                if (status.getActivityLevel() == Replicator.ActivityLevel.STOPPED) {
-                    stopped.countDown();
-                }
-            }
-        });
+                if (status.getActivityLevel() == Replicator.ActivityLevel.STOPPED) { stopped.countDown(); }
+            });
         repl.start();
         assertTrue(offline.await(10, TimeUnit.SECONDS));
         assertTrue(stopped.await(10, TimeUnit.SECONDS));
@@ -59,22 +50,14 @@ public class ReplicatorOfflineTest extends BaseReplicatorTest {
     @Test
     public void testStartSingleShotReplicatorInOffline() throws URISyntaxException, InterruptedException {
         Endpoint endpoint = getRemoteTargetEndpoint();
-        Replicator repl = new Replicator(makeConfig(
-                true,
-                false,
-                false,
-                endpoint
-        ));
+        Replicator repl = new Replicator(makeConfig(true, false, false, endpoint));
         final CountDownLatch stopped = new CountDownLatch(1);
-        ListenerToken token = repl.addChangeListener(executor, new ReplicatorChangeListener() {
-            @Override
-            public void changed(ReplicatorChange change) {
+        ListenerToken token = repl.addChangeListener(
+            executor,
+            change -> {
                 Replicator.Status status = change.getStatus();
-                if (status.getActivityLevel() == Replicator.ActivityLevel.STOPPED) {
-                    stopped.countDown();
-                }
-            }
-        });
+                if (status.getActivityLevel() == Replicator.ActivityLevel.STOPPED) { stopped.countDown(); }
+            });
         repl.start();
         assertTrue(stopped.await(10, TimeUnit.SECONDS));
         repl.removeChangeListener(token);
@@ -83,16 +66,8 @@ public class ReplicatorOfflineTest extends BaseReplicatorTest {
     @Test
     public void testDocumentChangeListenerToken() throws Exception {
         Endpoint endpoint = getRemoteTargetEndpoint();
-        Replicator repl = new Replicator(makeConfig(
-                true,
-                false,
-                false,
-                endpoint
-        ));
-        ListenerToken token = repl.addDocumentReplicationListener(new DocumentReplicationListener() {
-            @Override
-            public void replication(@NonNull DocumentReplication replication) { }
-        });
+        Replicator repl = new Replicator(makeConfig(true, false, false, endpoint));
+        ListenerToken token = repl.addDocumentReplicationListener(replication -> { });
         assertNotNull(token);
 
         thrown.expect(IllegalArgumentException.class);
@@ -105,12 +80,7 @@ public class ReplicatorOfflineTest extends BaseReplicatorTest {
     @Test
     public void testChangeListenerEmptyArg() throws Exception {
         Endpoint endpoint = getRemoteTargetEndpoint();
-        Replicator repl = new Replicator(makeConfig(
-                true,
-                false,
-                true,
-                endpoint
-        ));
+        Replicator repl = new Replicator(makeConfig(true, false, true, endpoint));
 
         thrown.expect(IllegalArgumentException.class);
         repl.addChangeListener(null);
@@ -126,23 +96,17 @@ public class ReplicatorOfflineTest extends BaseReplicatorTest {
         Replicator repl = new Replicator(config);
         final CountDownLatch offline = new CountDownLatch(2);
         final CountDownLatch stopped = new CountDownLatch(1);
-        ListenerToken token = repl.addChangeListener(executor, new ReplicatorChangeListener() {
-            @Override
-            public void changed(ReplicatorChange change) {
+        ListenerToken token = repl.addChangeListener(
+            executor,
+            change -> {
                 Replicator.Status status = change.getStatus();
                 if (status.getActivityLevel() == Replicator.ActivityLevel.OFFLINE) {
                     offline.countDown();
-                    if (offline.getCount() == 0) {
-                        change.getReplicator().stop();
-                    } else {
-                        change.getReplicator().networkReachable();
-                    }
+                    if (offline.getCount() == 0) { change.getReplicator().stop(); }
+                    else { change.getReplicator().networkReachable(); }
                 }
-                if (status.getActivityLevel() == Replicator.ActivityLevel.STOPPED) {
-                    stopped.countDown();
-                }
-            }
-        });
+                if (status.getActivityLevel() == Replicator.ActivityLevel.STOPPED) { stopped.countDown(); }
+            });
         repl.start();
         assertTrue(offline.await(10, TimeUnit.SECONDS));
         assertTrue(stopped.await(10, TimeUnit.SECONDS));
