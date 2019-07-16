06bd682c1db624d01136b060e20d2436f0c41d30
Pasin Suriyentrakorn <pasin@couchbase.com>
2019-07-15 18:14:01 -0700

    Refactor ConsoleLogger (#1929)
    This change is done as the core logic has been extracted to the AbstractConsoleLogger class per https://github.com/couchbase/couchbase-lite-java/pull/121.

--- lib/src/main/java/com/couchbase/lite/ConsoleLogger.java
+++ lib/src/main/java/com/couchbase/lite/ConsoleLogger.java
@@ -20,85 +20,14 @@ package com.couchbase.lite;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
-import java.util.EnumSet;
-
-import com.couchbase.lite.internal.core.C4Log;
-import com.couchbase.lite.internal.utils.Preconditions;
-
-
 /**
  * A class for sending log messages to Android's system log (aka logcat).  This is useful
  * for debugging during development, but is recommended to be disabled in production (the
  * file logger is both more durable and more efficient)
  */
-public final class ConsoleLogger implements Logger {
-    private LogLevel logLevel = LogLevel.WARNING;
-    private EnumSet<LogDomain> logDomains = EnumSet.of(LogDomain.ALL);
-
-    // Singleton instance accessible from Log.getConsole()
-    ConsoleLogger() { }
-
-    /**
-     * Gets the domains that will be considered for writing to
-     * the Android system log
-     *
-     * @return The currently active domains
-     */
-    @NonNull
-    public EnumSet<LogDomain> getDomains() {
-        return logDomains;
-    }
-
-    /**
-     * Sets the domains that will be considered for writing to
-     * the Android system log
-     *
-     * @param domains The domains to make active
-     */
-    public void setDomains(@NonNull EnumSet<LogDomain> domains) {
-        Preconditions.checkArgNotNull(domains, "domains");
-        logDomains = domains;
-    }
-
-    private void setCallbackLevel(@NonNull LogLevel level) {
-        Preconditions.checkArgNotNull(level, "level");
-
-        LogLevel callbackLevel = level;
-        final Logger custom = Database.log.getCustom();
-        if ((custom != null) && (custom.getLevel().compareTo(callbackLevel) < 0)) {
-            callbackLevel = custom.getLevel();
-        }
-
-        C4Log.setCallbackLevel(callbackLevel.getValue());
-    }
-
-    @NonNull
+public final class ConsoleLogger extends AbstractConsoleLogger {
     @Override
-    public LogLevel getLevel() { return logLevel; }
-
-    /**
-     * Sets the overall logging level that will be written to
-     * the Android system log
-     *
-     * @param level The maximum level to include in the logs
-     */
-    public void setLevel(@NonNull LogLevel level) {
-        Preconditions.checkArgNotNull(level, "level");
-
-        if (logLevel == level) { return; }
-
-        logLevel = level;
-        setCallbackLevel(level);
-    }
-
-    @Override
-    public void log(LogLevel level, @NonNull LogDomain domain, @NonNull String message) {
-        if (level.compareTo(logLevel) < 0
-            || (!logDomains.contains(domain)
-            && !logDomains.contains(LogDomain.ALL))) {
-            return;
-        }
-
+    protected void doLog(LogLevel level, @NonNull LogDomain domain, @NonNull String message) {
         switch (level) {
             case DEBUG:
                 Log.d("CouchbaseLite/" + domain.toString(), message);
