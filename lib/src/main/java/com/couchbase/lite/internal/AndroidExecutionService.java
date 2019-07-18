b3e021a352ff362fd0806b148b487581cdc3e769
Pasin Suriyentrakorn <pasin@couchbase.com>
2019-07-17 18:54:30 -0700

    Refactor AndroidExecutionService (#1928)
    * AndroidExecutionService extends from the base implementation class (AbstractExecutionService).
    
    * This change is done corresponding to the implementation of JavaExecutionService (https://github.com/couchbase/couchbase-lite-java/pull/120).
    
    * Used Cancellable instead of Object for Delayed Task
    
    * Used Preconditions to check null parameters.

--- lib/src/main/java/com/couchbase/lite/internal/AndroidExecutionService.java
+++ lib/src/main/java/com/couchbase/lite/internal/AndroidExecutionService.java
@@ -22,133 +22,47 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 
-import java.util.ArrayDeque;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.TimeUnit;
 
+import com.couchbase.lite.internal.utils.Preconditions;
 
-public final class AndroidExecutionService implements ExecutionService {
+/**
+ * ExecutionService for Android.
+ */
+public final class AndroidExecutionService extends AbstractExecutionService {
+    private static class CancellableTask implements Cancellable {
+        private Handler handler;
+        private Runnable task;
 
-    // thin wrapper around the AsyncTask's THREAD_POOL_EXECUTOR
-    private static class ConcurrentExecutor implements CloseableExecutor {
-        private final Executor executor;
-        private CountDownLatch stopLatch;
-        private int running;
-
-        private ConcurrentExecutor(Executor executor) { this.executor = executor; }
-
-        @Override
-        public synchronized void execute(Runnable task) {
-            if (stopLatch != null) { throw new RejectedExecutionException("Executor has been stopped"); }
-
-            running++;
-
-            executor.execute(() -> {
-                try { task.run(); }
-                finally { finishTask(); }
-            });
+        private CancellableTask(@NonNull Handler handler, @NonNull Runnable task) {
+            Preconditions.checkArgNotNull(handler, "handler");
+            Preconditions.checkArgNotNull(task, "task");
+            this.handler = handler;
+            this.task = task;
         }
 
         @Override
-        public boolean stop(long timeout, @NonNull TimeUnit unit) {
-            final CountDownLatch latch;
-            synchronized (this) {
-                if (stopLatch == null) { stopLatch = new CountDownLatch(running); }
-                if (running <= 0) { return true; }
-                latch = stopLatch;
-            }
-
-            try { return latch.await(timeout, unit); }
-            catch (InterruptedException ignore) { }
-
-            return false;
-        }
-
-        private void finishTask() {
-            final CountDownLatch latch;
-            synchronized (this) {
-                running--;
-                latch = stopLatch;
-            }
-
-            if (latch != null) { latch.countDown(); }
+        public void cancel() {
+            handler.removeCallbacks(task);
         }
     }
-
-    // Patterned after AsyncTask's executor
-    private static class SerialExecutor implements CloseableExecutor {
-        private final ArrayDeque<Runnable> tasks = new ArrayDeque<>();
-        private final Executor executor;
-        private CountDownLatch stopLatch;
-
-        private Runnable running;
-
-        private SerialExecutor(Executor executor) { this.executor = executor; }
-
-        public synchronized void execute(Runnable task) {
-            if (stopLatch != null) { throw new RejectedExecutionException("Executor has been stopped"); }
-
-            tasks.offer(() -> {
-                try { task.run(); }
-                finally { scheduleNext(); }
-            });
-
-            if (running == null) { scheduleNext(); }
-        }
-
-        @Override
-        public boolean stop(long timeout, @NonNull TimeUnit unit) {
-            final CountDownLatch latch;
-            final int n;
-            synchronized (this) {
-                n = (running == null) ? 0 : tasks.size() + 1;
-
-                if (stopLatch == null) { stopLatch = new CountDownLatch(n); }
-
-                if (n <= 0) { return true; }
-
-                latch = stopLatch;
-            }
-
-            try { return latch.await(timeout, unit); }
-            catch (InterruptedException ignore) { }
-
-            return false;
-        }
-
-        private synchronized void scheduleNext() {
-            if (stopLatch != null) { stopLatch.countDown(); }
-
-            running = tasks.poll();
-
-            if (running != null) { executor.execute(running); }
-        }
-    }
-
 
     private final Handler mainHandler;
     private final Executor mainThreadExecutor;
-    private final CloseableExecutor concurrentExecutor;
 
     public AndroidExecutionService() {
         mainHandler = new Handler(Looper.getMainLooper());
-
         mainThreadExecutor = mainHandler::post;
-
-        concurrentExecutor = new ConcurrentExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
+
+    @NonNull
+    @Override
+    public Executor getThreadPoolExecutor() { return AsyncTask.THREAD_POOL_EXECUTOR; }
 
     @NonNull
     @Override
     public Executor getMainExecutor() { return mainThreadExecutor; }
-
-    public CloseableExecutor getSerialExecutor() { return new SerialExecutor(AsyncTask.THREAD_POOL_EXECUTOR); }
-
-    @NonNull
-    @Override
-    public CloseableExecutor getConcurrentExecutor() { return concurrentExecutor; }
 
     /**
      * This runs a task on Android's main thread for just long enough to enough to enqueue the passed task
@@ -159,32 +73,30 @@ public final class AndroidExecutionService implements ExecutionService {
      * @param delayMs  delay before posting the task.  There may be additional queue delays in the executor.
      * @param executor an executor on which to execute the task.
      * @param task     the task to be executed.
+     * @return a cancellable task
      */
     @NonNull
     @Override
-    public Runnable postDelayedOnExecutor(long delayMs, @NonNull Executor executor, @NonNull Runnable task) {
-        if (null == task) { throw new IllegalArgumentException("Task may not be null"); }
-        if (null == executor) { throw new IllegalArgumentException("Executor may not be null"); }
-
+    public Cancellable postDelayedOnExecutor(long delayMs, @NonNull Executor executor, @NonNull Runnable task) {
+        Preconditions.checkArgNotNull(executor, "executor");
+        Preconditions.checkArgNotNull(task, "task");
         final Runnable delayedTask = () -> {
             try { executor.execute(task); }
             catch (RejectedExecutionException ignored) { }
         };
-
         mainHandler.postDelayed(delayedTask, delayMs);
-
-        return delayedTask;
+        return new CancellableTask(mainHandler, delayedTask);
     }
 
     /**
      * Best effort, delete the passed task (obtained from postDelayedOnExecutor, above)
      * from the wait queue.  If it is already in the Executor, well, there you go.
      *
-     * @param task the task to be executed.
+     * @param cancellableTask returned by a previous call to postDelayedOnExecutor.
      */
     @Override
-    public void cancelDelayedTask(@NonNull Runnable task) {
-        if (null == task) { throw new IllegalArgumentException("Task may not be null"); }
-        mainHandler.removeCallbacks(task);
+    public void cancelDelayedTask(@NonNull Cancellable cancellableTask) {
+        Preconditions.checkArgNotNull(cancellableTask, "cancellableTask");
+        cancellableTask.cancel();
     }
 }
