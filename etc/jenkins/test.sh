d79adce334dcf339413197cef0fc42bb54127a98
G. Blake Meike <blake.meike@couchbase.com>
2019-08-29 10:19:27 -0700

    Fix test script
    Change-Id: I10afcfa7e86065cc9376bad9a0d3609ecc756da6

--- etc/jenkins/test.sh
+++ etc/jenkins/test.sh
@@ -25,10 +25,10 @@ if [ -z "REPORTS" ]; then
     usage
 fi
 
-echo "======== Run automated tests"
+echo "======== Run automated tests on device: ${ANDROID_SERIAL}"
 ./gradlew ciTest --info --console=plain -PautomatedTests=true -PbuildNumber="${BUILD_NUMBER}" || exit 1
 
-echo "======== Copy test reports ========"
-cp -rp lib/build/reports/* "${REPORTS}/"
+echo "======== Copy test reports"
+cp -rp test/build/reports/* "${REPORTS}/"
 
 echo "======== TEST COMPLETE"
