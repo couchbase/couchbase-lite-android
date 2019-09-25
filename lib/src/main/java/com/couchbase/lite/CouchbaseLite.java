ed2d89de0f82202b6db546fcc7e0b9ff785b79c1
G. Blake Meike <blake.meike@couchbase.com>
2019-09-24 20:36:44 -0700

    Clean up initialization, esp for logging.
    Change-Id: I002ecbc8e9cf75d0a1d61fb105d5ef81796dab1b

--- lib/src/main/java/com/couchbase/lite/CouchbaseLite.java
+++ lib/src/main/java/com/couchbase/lite/CouchbaseLite.java
@@ -17,6 +17,7 @@ package com.couchbase.lite;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,40 +36,47 @@ import org.json.JSONObject;
 
 import com.couchbase.lite.internal.AndroidExecutionService;
 import com.couchbase.lite.internal.ExecutionService;
+import com.couchbase.lite.internal.core.CBLVersion;
 import com.couchbase.lite.internal.fleece.MValue;
 import com.couchbase.lite.internal.support.Log;
 import com.couchbase.lite.internal.utils.Preconditions;
 
 
 public final class CouchbaseLite {
+    // Utility class
+    private CouchbaseLite() {}
+
     private static final String LITECORE_JNI_LIBRARY = "LiteCoreJNI";
+    private static final String MVALUE_DELEGATE_CLASS = "com.couchbase.lite.MValueDelegate";
 
     private static final AtomicReference<ExecutionService> EXECUTION_SERVICE = new AtomicReference<>();
     private static final AtomicReference<SoftReference<Context>> CONTEXT = new AtomicReference<>();
 
     private static final AtomicBoolean INITIALIZED = new AtomicBoolean(false);
 
-    // Utility class
-    private CouchbaseLite() {}
-
     /**
-     * Initialize CouchbaseLite library. This method MUST be called before
-     * using CouchbaseLite.
+     * Initialize CouchbaseLite library. This method MUST be called before using CouchbaseLite.
      */
     public static void init(@NonNull Context ctxt) {
         Preconditions.checkArgNotNull(ctxt, "context");
 
         if (INITIALIZED.getAndSet(true)) { return; }
 
+        System.loadLibrary(LITECORE_JNI_LIBRARY);
+
+        initMValue();
+
         CONTEXT.set(new SoftReference<>(ctxt.getApplicationContext()));
 
-        loadSystemLibrary(LITECORE_JNI_LIBRARY);
-
         loadErrorMessages(ctxt);
+
+        Log.initLogging(BuildConfig.DEBUG);
+        Log.i(LogDomain.DATABASE, "Couchbase Lite initialized: " + CBLVersion.getVersionInfo());
     }
 
     /**
-     * This method is for internal used only and will be removed in the future release.
+     * This method is not part of the public API.
+     * It will be removed in a future release.
      */
     public static ExecutionService getExecutionService() {
         ExecutionService executionService = EXECUTION_SERVICE.get();
@@ -79,12 +87,16 @@ public final class CouchbaseLite {
         return executionService;
     }
 
+    static void requireInit(String message) {
+        if (!INITIALIZED.get()) {
+            throw new IllegalStateException(message + ".  Did you forget to call CouchbaseLite.init()?");
+        }
+    }
+
     @NonNull
     static Context getContext() {
+        requireInit("Application context not initialized");
         final SoftReference<Context> contextRef = CONTEXT.get();
-        if (contextRef == null) {
-            throw new IllegalStateException("Null context.  Did you forget to call CouchbaseLite.init()?");
-        }
 
         final Context ctxt = contextRef.get();
         if (ctxt == null) { throw new IllegalStateException("Context is null"); }
@@ -93,27 +105,31 @@ public final class CouchbaseLite {
     }
 
     static String getDbDirectoryPath() {
+        requireInit("Database directory not initialized");
         return getContext().getFilesDir().getAbsolutePath();
     }
 
-    static String getTmpDirectory(@NonNull String name) {
-        return getTmpDirectory(getContext().getCacheDir().getAbsolutePath(), name);
-    }
+    static String getTmpDirectory(@NonNull String name) { return verifyDir(getContext().getExternalFilesDir(name)); }
 
-    static String getTmpDirectory(String root, String name) {
-        final File dir = new File(root, name);
+    static String getTmpDirectory(String root, String name) { return verifyDir(new File(root, name)); }
 
+    @Nullable
+    private static String verifyDir(@Nullable File dir) {
+        if (dir == null) { return null; }
         final String path = dir.getAbsolutePath();
-        if ((dir.exists() || dir.mkdirs()) && dir.isDirectory()) { return path; }
-
+        if ((dir.exists() && dir.isDirectory()) || dir.mkdirs()) { return path; }
         throw new IllegalStateException("Cannot create or access temp directory at " + path);
     }
 
-    private static void loadSystemLibrary(String libName) {
-        if (!load(libName)) { Log.e(LogDomain.DATABASE, "Cannot load native library"); }
-        else { Log.v(LogDomain.DATABASE, "Successfully load native library: 'LiteCoreJNI' and 'sqlite3'"); }
-
-        initMValue();
+    private static void initMValue() {
+        try {
+            final Constructor ctor = Class.forName(MVALUE_DELEGATE_CLASS).getDeclaredConstructor();
+            ctor.setAccessible(true);
+            MValue.registerDelegate((MValue.Delegate) ctor.newInstance());
+        }
+        catch (Exception e) {
+            throw new IllegalStateException("Cannot initialize MValue delegate", e);
+        }
     }
 
     private static void loadErrorMessages(Context ctxt) {
@@ -132,26 +148,5 @@ public final class CouchbaseLite {
         }
 
         CBLError.setErrorMessages(errorMessages);
-    }
-
-    // TODO: Need to update for CBL Java.
-    private static boolean load(String libName) {
-        try {
-            System.loadLibrary(libName);
-            return true;
-        }
-        catch (UnsatisfiedLinkError ignore) { }
-        return false;
-    }
-
-    private static void initMValue() {
-        try {
-            final Constructor c = Class.forName("com.couchbase.lite.MValueDelegate").getDeclaredConstructor();
-            c.setAccessible(true);
-            MValue.registerDelegate((MValue.Delegate) c.newInstance());
-        }
-        catch (Exception e) {
-            throw new IllegalStateException("Cannot initialize MValue delegate", e);
-        }
     }
 }
