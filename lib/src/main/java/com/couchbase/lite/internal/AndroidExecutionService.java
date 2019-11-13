f70aa4d6932d71f5a97b8e3c49dfc1d46e18feba
G. Blake Meike <blake.meike@couchbase.com>
2019-11-13 15:00:22 -0800

    Add instrumented execution service to help diagnose RejectedExecution issue
    Change-Id: I26195c38a4b166c8bfcf1b04847376b8f40cce4b

--- lib/src/main/java/com/couchbase/lite/internal/AndroidExecutionService.java
+++ lib/src/main/java/com/couchbase/lite/internal/AndroidExecutionService.java
@@ -23,13 +23,55 @@ import android.os.Looper;
 import android.support.annotation.NonNull;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import com.couchbase.lite.LogDomain;
+import com.couchbase.lite.internal.support.Log;
 
 
 public final class AndroidExecutionService implements ExecutionService {
+    private static final LogDomain DOMAIN = LogDomain.DATABASE;
+    private static final Object DUMP_LOCK = new Object();
+    private static long lastDump;
+
+    public static class InstrumentedTask implements Runnable {
+        private final Exception origin = new Exception();
+        private final Runnable task;
+        private final Runnable onComplete;
+
+        private final long createdAt = System.currentTimeMillis();
+        private long startedAt;
+        private long finishedAt;
+        private long completedAt;
+
+        public InstrumentedTask(Runnable task, Runnable onComplete) {
+            this.task = task;
+            this.onComplete = onComplete;
+        }
+
+        public void run() {
+            try {
+                startedAt = System.currentTimeMillis();
+                task.run();
+                finishedAt = System.currentTimeMillis();
+            }
+            finally {
+                onComplete.run();
+            }
+            completedAt = System.currentTimeMillis();
+        }
+
+        public String toString() {
+            return "task[" + createdAt + "," + startedAt + "," + finishedAt + "," + completedAt + " @" + task + "]";
+        }
+    }
 
     // thin wrapper around the AsyncTask's THREAD_POOL_EXECUTOR
     private static class ConcurrentExecutor implements CloseableExecutor {
@@ -40,15 +82,17 @@ public final class AndroidExecutionService implements ExecutionService {
         private ConcurrentExecutor(Executor executor) { this.executor = executor; }
 
         @Override
-        public synchronized void execute(Runnable task) {
+        public synchronized void execute(@NonNull Runnable task) {
             if (stopLatch != null) { throw new RejectedExecutionException("Executor has been stopped"); }
 
-            running++;
-
-            executor.execute(() -> {
-                try { task.run(); }
-                finally { finishTask(); }
-            });
+            try {
+                executor.execute(new InstrumentedTask(task, this::finishTask));
+                running++;
+            }
+            catch (RejectedExecutionException e) {
+                dumpServiceState(executor, e, "size: " + running);
+                throw e;
+            }
         }
 
         @Override
@@ -79,21 +123,20 @@ public final class AndroidExecutionService implements ExecutionService {
 
     // Patterned after AsyncTask's executor
     private static class SerialExecutor implements CloseableExecutor {
-        private final ArrayDeque<Runnable> tasks = new ArrayDeque<>();
+        private final Deque<InstrumentedTask> tasks = new ArrayDeque<>();
         private final Executor executor;
+
+        // If non-null, this executor has been stopped.
         private CountDownLatch stopLatch;
 
-        private Runnable running;
+        private InstrumentedTask running;
 
         private SerialExecutor(Executor executor) { this.executor = executor; }
 
-        public synchronized void execute(Runnable task) {
+        public synchronized void execute(@NonNull Runnable task) {
             if (stopLatch != null) { throw new RejectedExecutionException("Executor has been stopped"); }
 
-            tasks.offer(() -> {
-                try { task.run(); }
-                finally { scheduleNext(); }
-            });
+            tasks.offerLast(new InstrumentedTask(task, this::scheduleNext));
 
             if (running == null) { scheduleNext(); }
         }
@@ -121,9 +164,36 @@ public final class AndroidExecutionService implements ExecutionService {
         private synchronized void scheduleNext() {
             if (stopLatch != null) { stopLatch.countDown(); }
 
+            final InstrumentedTask prev = running;
             running = tasks.poll();
+            if (running == null) { return; }
 
-            if (running != null) { executor.execute(running); }
+            try { executor.execute(running); }
+            catch (RejectedExecutionException e) {
+                dumpExecutorState(e, prev, running, tasks);
+                running = null;
+            }
+        }
+
+        private void dumpExecutorState(
+            RejectedExecutionException ex,
+            InstrumentedTask prev,
+            InstrumentedTask current,
+            Deque<InstrumentedTask> tasks) {
+            if (throttled()) { return; }
+
+            dumpServiceState(executor, ex, "size: " + tasks.size());
+
+            Log.d(DOMAIN, "==== Serial Executor status: " + this);
+
+            if (prev != null) { Log.d(DOMAIN, "== Previous task: " + prev, prev.origin); }
+
+            if (current != null) { Log.d(DOMAIN, "== Current task: " + current, current.origin); }
+
+            final ArrayList<InstrumentedTask> waiting = new ArrayList<>(tasks);
+            Log.d(DOMAIN, "== Waiting tasks: " + waiting.size());
+            int n = 0;
+            for (InstrumentedTask t : waiting) { Log.d(DOMAIN, "@" + (++n) + ": " + t, t.origin); }
         }
     }
 
@@ -186,5 +256,42 @@ public final class AndroidExecutionService implements ExecutionService {
     public void cancelDelayedTask(@NonNull Runnable task) {
         if (null == task) { throw new IllegalArgumentException("Task may not be null"); }
         mainHandler.removeCallbacks(task);
+    }
+
+    static void dumpServiceState(Executor ex, Exception e, String msg) { dumpServiceState(ex, e, null, msg); }
+
+    private static void dumpServiceState(Executor ex, Exception e, Exception origin, String msg) {
+        if (throttled()) { return; }
+
+        Log.d(LogDomain.DATABASE, "!!!! Catastrophic failure on executor " + ex + ": " + msg, e);
+        if (origin != null) { Log.d(DOMAIN, "!! Origin: ", origin); }
+
+        final Map<Thread, StackTraceElement[]> stackTraces = Thread.getAllStackTraces();
+        Log.d(DOMAIN, "==== Threads: " + stackTraces.size());
+        for (Map.Entry<Thread, StackTraceElement[]> stack : stackTraces.entrySet()) {
+            Log.d(DOMAIN, "== Thread: " + stack.getKey());
+            for (StackTraceElement frame : stack.getValue()) { Log.d(DOMAIN, "      at " + frame); }
+        }
+
+        if (!(ex instanceof ThreadPoolExecutor)) { return; }
+
+        final ArrayList<Runnable> waiting = new ArrayList<>(((ThreadPoolExecutor) ex).getQueue());
+        Log.d(DOMAIN, "==== Executor queue: " + waiting.size());
+        int n = 0;
+        for (Runnable r : waiting) {
+            final Exception orig = (!(r instanceof InstrumentedTask)) ? null : ((InstrumentedTask) r).origin;
+            if (orig == null) { Log.d(DOMAIN, "@" + (n++) + ": " + r); }
+            else { Log.d(DOMAIN, "@" + (n++) + ": " + r, orig); }
+        }
+    }
+
+    private static boolean throttled() {
+        final long now = System.currentTimeMillis();
+        synchronized (DUMP_LOCK) {
+            // don't dump any more than once a second
+            if ((now - lastDump) < 1000) { return true; }
+            lastDump = now;
+        }
+        return false;
     }
 }
