142be550f465b856456d8032e9bdfe966cd7957b
G. Blake Meike <blake.meike@couchbase.com>
2019-12-16 13:38:40 -0800

    New jenkins build process
    Change-Id: I38575bb4b3b20b97de10924c0d937db5879d32f8

--- etc/jenkins/build.sh
+++ etc/jenkins/build.sh
@@ -9,23 +9,44 @@ MAVEN_URL="http://172.23.121.218/maven2/cimaven"
 
 
 function usage() {
-    echo "Usage: $0 <build number>"
+    echo "Usage: $0 <sdk path> <build number>"
     exit 1
 }
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     usage
 fi
 
-BUILD_NUMBER="$1"
+SDK_HOME="$1"
+if [ -z "$SDK_HOME" ]; then
+    usage
+fi
+
+BUILD_NUMBER="$2"
 if [ -z "$BUILD_NUMBER" ]; then
     usage
 fi
 
-echo "======== Build Couchbase Lite Android, Community Edition v`cat ../version.txt`-${BUILD_NUMBER}"
+SDK_MGR="${SDK_HOME}/tools/bin/sdkmanager"
+
+echo "======== BUILD Couchbase Lite Android, Community Edition v`cat ../version.txt`-${BUILD_NUMBER}"
+
+echo "======== Install Toolchain"
+yes | ${SDK_MGR} --licenses > /dev/null 2>&1
+${SDK_MGR} --install 'cmake;3.10.2.4988404'
+${SDK_MGR} --install 'ndk;20.1.5948944'
+
+# The Jenkins script has already put passwords into local.properties
+cat <<EOF >> local.properties
+sdk.dir=${SDK_HOME}
+ndk.dir=${SDK_HOME}/ndk/20.1.5948944
+cmake.dir=${SDK_HOME}/cmake/3.10.2.4988404
+EOF
+
+echo "======== Build"
 ./gradlew ciCheck -PbuildNumber="${BUILD_NUMBER}" || exit 1
 
-echo "======== Publish build candidates to CI maven"
+echo "======== Publish artifacts"
 ./gradlew ciPublish -PbuildNumber="${BUILD_NUMBER}" -PmavenUrl="${MAVEN_URL}" || exit 1
 
 echo "======== BUILD COMPLETE"
